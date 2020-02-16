dae5e871d3b9ae7d55f48e847abb205af048a9a9
wojciechkepka <wojciech.kepka@protonmail.com>
2020-02-16 09:27:31 +0100

    Dodanie projektu

---
+++ konwerter_jednostek_Z412_Wojciech_Kepka/konwerter_jednostek_Z412_Wojciech_Kepka/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonwerterJednostek
+{
+    enum Jednostka
+    {
+        Celsjusz,
+        Farenheit,
+        Kilometry,
+        Mile,
+        Kilogramy,
+        Funty
+    }
+    class Konwerter
+    {
+        double inp_v, out_v;
+        Jednostka inp_u, out_u;
+        bool calculated = false;
+
+        Konwerter()
+        {
+            Console.WriteLine("Konwerter jednosterk\n#########################################");
+            Console.WriteLine("\nKonwertuje wybraną jednostkę na jej odpowiednik\n");
+            Console.WriteLine("Dostępne Jednostki:");
+            Console.WriteLine("\tkg\t(Kilogramy)");
+            Console.WriteLine("\tlb\t(Funty)");
+            Console.WriteLine("\tc\t(Stopnie Celsjusza)");
+            Console.WriteLine("\tf\t(Stopnie Farenheita)");
+            Console.WriteLine("\tkm\t(Kilometry)");
+            Console.WriteLine("\tmi\t(Mile)");
+        }
+
+        bool SetInpVal(string user_inp)
+        {
+            try
+            {
+                this.inp_v = Double.Parse(user_inp);
+                return true;
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine($"{user_inp} nie jest poprawną liczbą. Spróbuj ponownie");
+                return false;
+            }
+        }
+
+        bool SetInpUnit(string user_inp)
+        {
+            switch (user_inp)
+            {
+                case "kg":
+                    inp_u = Jednostka.Kilogramy;
+                    return true;
+                case "lb":
+                    inp_u = Jednostka.Funty;
+                    return true;
+                case "c":
+                    inp_u = Jednostka.Celsjusz;
+                    return true;
+                case "f":
+                    inp_u = Jednostka.Farenheit;
+                    return true;
+                case "km":
+                    inp_u = Jednostka.Kilometry;
+                    return true;
+                case "mi":
+                    inp_u = Jednostka.Mile;
+                    return true;
+                default:
+                    Console.WriteLine($"Podana jednostka {user_inp} jest nieobsługiwana. Spróbuj ponownie.");
+                    return false;
+            }
+        }
+        string OutValUnit()
+        {
+            switch (out_u)
+            {
+                case Jednostka.Celsjusz:
+                    return "c";
+                case Jednostka.Farenheit:
+                    return "f";
+                case Jednostka.Kilogramy:
+                    return "kg";
+                case Jednostka.Funty:
+                    return "lb";
+                case Jednostka.Kilometry:
+                    return "km";
+                case Jednostka.Mile:
+                    return "mi";
+                default:
+                    return "";
+            }
+        }
+        void Convert()
+        {
+            switch (inp_u)
+            {
+                case Jednostka.Celsjusz:
+                    out_v = inp_v * (9 / 5) + 32;
+                    out_u = Jednostka.Farenheit;
+                    break;
+                case Jednostka.Farenheit:
+                    out_v = (inp_v - 32) * 5/9;
+                    out_u = Jednostka.Celsjusz;
+                    break;
+                case Jednostka.Kilogramy:
+                    out_v = inp_v * 2.2046;
+                    out_u = Jednostka.Funty;
+                    break;
+                case Jednostka.Funty:
+                    out_v = inp_v / 2.2046;
+                    out_u = Jednostka.Kilogramy;
+                    break;
+                case Jednostka.Kilometry:
+                    out_v = inp_v * 0.621371192;
+                    out_u = Jednostka.Mile;
+                    break;
+                case Jednostka.Mile:
+                    out_v = inp_v / 0.621371192;
+                    out_u = Jednostka.Kilometry;
+                    break;
+                default:
+                    break;
+
+            }
+            calculated = true;
+        }
+        void PrintOut()
+        {
+            if (!calculated) { Convert(); }
+            Console.WriteLine($"{inp_v} {inp_u} = {out_v} {out_u}");
+        }
+        void Clear()
+        {
+            inp_v = default; inp_u = default; out_v = default; out_u = default;
+            calculated = false;
+        }
+        static void Main(string[] args)
+        {
+            Konwerter conv = new Konwerter();
+
+            // Ustawiamy wartość wejściową
+            while (true) 
+            {
+                Console.WriteLine("Podaj wartość wejściową: ");
+                string user_inp_v = Console.ReadLine();
+                if (conv.SetInpVal(user_inp_v)) { break; }
+            }
+
+            // Ustawiamy jednostkę wejściową
+            while (true)
+            {
+                Console.WriteLine("Podaj jednostkę: ");
+                string user_inp_u = Console.ReadLine();
+                if (conv.SetInpUnit(user_inp_u)) { break; }
+            }
+
+            conv.Convert();
+            conv.PrintOut();
+        }
+    }
+}
---
+++ konwerter_jednostek_Z412_Wojciech_Kepka/konwerter_jednostek_Z412_Wojciech_Kepka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("konwerter_jednostek_Z412_Wojciech_Kepka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("konwerter_jednostek_Z412_Wojciech_Kepka")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("07b1f89a-fe49-4d03-b2b9-7f8b3a5f8ff7")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
