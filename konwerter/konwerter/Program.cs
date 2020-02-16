0b332f2e4c400de81bd5b2f312c5177ee8e72b33
Lukasz Kolodziejczyk <kolodziejczyk.lukasz@gmail.com>
2020-02-16 17:48:23 +0100

    Poprawa

--- konwerter/konwerter/Program.cs
+++ konwerter/konwerter/Program.cs
@@ -17,13 +17,19 @@ namespace konwerter
             Console.WriteLine();
             Console.WriteLine("Podaj jednostke z której chcesz przeliczyć: (Km / Mil / C / F / Kg / Funt)");            
             tmp1 = Console.ReadLine();
+            if(tmp1 != "Km" || tmp1 != "Mil" || tmp1 != "C" || tmp1 != "F" || tmp1 != "Kg" || tmp1 != "Funt")
+            {
+                Console.WriteLine("Podano niepoprawną wartość jednostki");
+                Console.WriteLine("Podaj poprawną wartość (Km / Mil / C / F / Kg / Funt)");
+                tmp1 = Console.ReadLine();
+            }
             if (tmp1 == "C")
             {
                 Console.WriteLine("Podaj wartoś do przeliczenia:");
                 tmp2 = double.Parse(Console.ReadLine());
                 if (tmp2 < -273.15)
                 {
-                    Console.WriteLine("Błąd wartość nie może byćmniejsza od zera absolutnego");
+                    Console.WriteLine("Błąd wartość nie może być mniejsza od zera absolutnego");
                     Console.WriteLine("Podaj poprawną wartość");
                     tmp2 = double.Parse(Console.ReadLine());
                 }
@@ -34,11 +40,27 @@ namespace konwerter
                 tmp2 = double.Parse(Console.ReadLine());
                 if (tmp2 < -459.67)
                 {
-                    Console.WriteLine("Błąd wartość nie może byćmniejsza od zera absolutnego");
+                    Console.WriteLine("Błąd wartość nie może być mniejsza od zera absolutnego");
                     Console.WriteLine("Podaj poprawną wartość");
                     tmp2 = double.Parse(Console.ReadLine());
                 }
             }
+            if (tmp1 == "Km" || tmp1 == "Mil" || tmp1 == "Kg" || tmp1 == "Funt")
+            {
+                Console.WriteLine("Podaj wartoś do przeliczenia:");
+                tmp2 = double.Parse(Console.ReadLine());
+                if (tmp2 <= 0) 
+                {
+                    Console.WriteLine("Błąd wartość nie może być mniejsza lub równa 0");
+                    Console.WriteLine("Podaj poprawną wartość większą od zera");
+                    tmp2 = double.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Podaj wartoś do przeliczenia:");
+                    tmp2 = double.Parse(Console.ReadLine());
+                }
+            }            
             Konwerter kalk1 = new Konwerter();
             kalk1.daj_dane(tmp2, tmp1);
             kalk1.konwersja();
