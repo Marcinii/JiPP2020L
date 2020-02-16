91f3e14011e6866a8450751e6b54f6acbc5865c7
l_kolodziejczyk <l_kolodziejczyk@WWSI-0423>
2020-02-16 11:15:04 +0100

    Poprawki

--- konwerter/konwerter/Konwerter.cs
+++ konwerter/konwerter/Konwerter.cs
@@ -6,5 +6,38 @@ namespace konwerter
 {
     class Konwerter
     {
+        double wartosc, wynik;
+        string typ;
+        public Konwerter(double wartosc, string typ) 
+        {
+            this.wartosc = wartosc; this.typ = typ;
+        }
+        public void konwersja()
+        {
+            if(typ == "C")
+            {
+
+            }
+            if(typ == "F")
+            {
+
+            }
+            if(typ == "Km")
+            {
+
+            }
+            if(typ == "Mil")
+            {
+
+            }
+            if(typ == "Kg")
+            {
+
+            }
+            if(typ == "Funt")
+            {
+
+            }
+        }
     }
 }
--- konwerter/konwerter/Program.cs
+++ konwerter/konwerter/Program.cs
@@ -6,7 +6,16 @@ namespace konwerter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double tmp1; string tmp2;
+            Console.WriteLine("============ KOWERTER JEDNOSTEK ============");
+            Console.WriteLine("Program konwertuje nastepne jednostki:");
+            Console.WriteLine("Kilogramy na Funty / Funty na Kilogramy");
+            Console.WriteLine("Kilogramy na Funty / Funty na Kilogramy");
+            Console.WriteLine("Stopnie C na F / F na C");
+            Console.WriteLine("Odległości Kilometry na Mile / Mile na Kilometry");
+            Console.WriteLine("Podaj wartoś do przeliczenia:");
+            Console.ReadLine()
+            Console.WriteLine("Podaj Jednostke: (Km / Mil / C / F / Kg / Funt");
         }
     }
 }
