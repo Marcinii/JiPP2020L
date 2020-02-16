ed53a9a66d1ee3ab48efd4ae4a10ec25f939ed7b
Lukasz Kolodziejczyk <kolodziejczyk.lukasz@gmail.com>
2020-02-16 17:36:38 +0100

    Poprawki

--- konwerter/konwerter/Program.cs
+++ konwerter/konwerter/Program.cs
@@ -6,18 +6,41 @@ namespace konwerter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("============ KOWERTER JEDNOSTEK ============");
-            Console.WriteLine("Program konwertuje nastepne jednostki:");
-            Console.WriteLine("Kilogramy na Funty / Funty na Kilogramy");
-            Console.WriteLine("Kilogramy na Funty / Funty na Kilogramy");
-            Console.WriteLine("Stopnie C na F / F na C");
-            Console.WriteLine("Odległości Kilometry na Mile / Mile na Kilometry");
-            Console.WriteLine("Podaj wartoś do przeliczenia:");
-            double tmp1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj Jednostke: (Km / Mil / C / F / Kg / Funt");
-            string tmp2 = Console.ReadLine();
+            double tmp2 = 0; string tmp1 = "brak";
+            Console.WriteLine("======================= KOWERTER JEDNOSTEK =======================");
+            Console.WriteLine("+     Program konwertuje nastepne jednostki:                     +");
+            Console.WriteLine("+     Kilogramy na Funty / Funty na Kilogramy                    +");
+            Console.WriteLine("+     Kilogramy na Funty / Funty na Kilogramy                    +");
+            Console.WriteLine("+     Stopnie C na F / F na C                                    +");
+            Console.WriteLine("+     Odległości Kilometry na Mile / Mile na Kilometry           +");
+            Console.WriteLine("==================================================================");
+            Console.WriteLine();
+            Console.WriteLine("Podaj jednostke z której chcesz przeliczyć: (Km / Mil / C / F / Kg / Funt)");            
+            tmp1 = Console.ReadLine();
+            if (tmp1 == "C")
+            {
+                Console.WriteLine("Podaj wartoś do przeliczenia:");
+                tmp2 = double.Parse(Console.ReadLine());
+                if (tmp2 < -273.15)
+                {
+                    Console.WriteLine("Błąd wartość nie może byćmniejsza od zera absolutnego");
+                    Console.WriteLine("Podaj poprawną wartość");
+                    tmp2 = double.Parse(Console.ReadLine());
+                }
+            }
+            if (tmp1 == "F")
+            {
+                Console.WriteLine("Podaj wartoś do przeliczenia:");
+                tmp2 = double.Parse(Console.ReadLine());
+                if (tmp2 < -459.67)
+                {
+                    Console.WriteLine("Błąd wartość nie może byćmniejsza od zera absolutnego");
+                    Console.WriteLine("Podaj poprawną wartość");
+                    tmp2 = double.Parse(Console.ReadLine());
+                }
+            }
             Konwerter kalk1 = new Konwerter();
-            kalk1.daj_dane(tmp1, tmp2);
+            kalk1.daj_dane(tmp2, tmp1);
             kalk1.konwersja();
             kalk1.daj_wynik();
         }
