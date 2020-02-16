23334e40ccf2dd2f66ffd94c63fdeee2e3db1764
p_szubiczuk <p_szubiczuk@WWSI-0419>
2020-02-16 11:09:48 +0100

    Pierwsza zmiana

---
+++ Konwenter-Z401-Piotr-Szubiczuk/Konwenter-Z401-Piotr-Szubiczuk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Konwenter_Z401_Piotr_Szubiczuk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
