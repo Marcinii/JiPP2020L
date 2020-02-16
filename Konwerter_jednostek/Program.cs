e74a5625b0c37b52db34af1dfaca08e43673bec2
Michal7322 <michal.zielinski@poczta.wwsi.edu.pl>
2020-02-16 15:05:13 +0100

    Projekt 1 probny Michal Zielinski

---
+++ Konwerter_jednostek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwerter_jednostek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konwerter jednostek");
+        }
+    }
+}
