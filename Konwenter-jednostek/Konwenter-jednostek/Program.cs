85bdd31663701d3602e7d9225f4e5d7a86c3a95f
mateuszg2521 <mateuszg2521@gmail.com>
2020-02-16 14:59:28 +0100

    Mateusz Garbacz - zadanie 1, commit początkowy

---
+++ Konwenter-jednostek/Konwenter-jednostek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwenter_jednostek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
