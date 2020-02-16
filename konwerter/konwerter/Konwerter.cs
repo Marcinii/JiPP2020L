54285b14beb4721a5e9af0f8c533eb8cd15b7f25
Lukasz Kolodziejczyk <kolodziejczyk.lukasz@gmail.com>
2020-02-16 16:58:51 +0100

    Poprawki programu usupełniłem metode konwersje

--- konwerter/konwerter/Konwerter.cs
+++ konwerter/konwerter/Konwerter.cs
@@ -7,8 +7,9 @@ namespace konwerter
     class Konwerter
     {
         double wartosc, wynik;
-        string typ;
-        public Konwerter(double wartosc, string typ) 
+        string typ, wyn_typ;
+        public Konwerter() { }
+        public void daj_dane(double wartosc, string typ) 
         {
             this.wartosc = wartosc; this.typ = typ;
         }
@@ -16,28 +17,38 @@ namespace konwerter
         {
             if(typ == "C")
             {
-
+                wyn_typ = "F";
+                wynik = (wartosc * 1.8) + 32;  
             }
             if(typ == "F")
             {
-
+                wyn_typ = "C";
+                wynik = (wartosc - 32) / 1.8;
             }
             if(typ == "Km")
             {
-
+                wyn_typ = "Mil";
+                wynik = wartosc * 0.62137;
             }
             if(typ == "Mil")
             {
-
+                wyn_typ = "Km";
+                wynik = wartosc / 0.62137;
             }
             if(typ == "Kg")
             {
-
+                wyn_typ = "Funta";
+                wynik = wartosc * 2.2046;
             }
             if(typ == "Funt")
             {
-
+                wyn_typ = "Kg";
+                wynik = wartosc / 2.2046;
             }
+        }
+        public void daj_wynik()
+        {
+            Console.WriteLine(wartosc.ToString() + " " + typ + " = " + wynik.ToString() + " " + wyn_typ);
         }
     }
 }
--- konwerter/konwerter/Program.cs
+++ konwerter/konwerter/Program.cs
@@ -6,7 +6,6 @@ namespace konwerter
     {
         static void Main(string[] args)
         {
-            double tmp1; string tmp2;
             Console.WriteLine("============ KOWERTER JEDNOSTEK ============");
             Console.WriteLine("Program konwertuje nastepne jednostki:");
             Console.WriteLine("Kilogramy na Funty / Funty na Kilogramy");
@@ -14,8 +13,13 @@ namespace konwerter
             Console.WriteLine("Stopnie C na F / F na C");
             Console.WriteLine("Odległości Kilometry na Mile / Mile na Kilometry");
             Console.WriteLine("Podaj wartoś do przeliczenia:");
-            Console.ReadLine()
+            double tmp1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Podaj Jednostke: (Km / Mil / C / F / Kg / Funt");
+            string tmp2 = Console.ReadLine();
+            Konwerter kalk1 = new Konwerter();
+            kalk1.daj_dane(tmp1, tmp2);
+            kalk1.konwersja();
+            kalk1.daj_wynik();
         }
     }
 }
