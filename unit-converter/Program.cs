a6677c6594dc06c8f34330898c914df6aefe593e
m_osiadacz <marcin.osiadacz@outlook.com>
2020-02-16 09:20:23 +0100

    Celsius to Farenheit conversion implemented.

--- unit-converter/Program.cs
+++ unit-converter/Program.cs
@@ -8,9 +8,50 @@ namespace unit_converter
 {
     class Program
     {
+        static void printMenu()
+        {
+            Console.WriteLine("1 - Celsius to Farenheit");
+            Console.WriteLine("2 - Farenheit to Celsius");
+            Console.WriteLine("3 - Kilometers to Miles");
+            Console.WriteLine("4 - Miles to Kilometers");
+            Console.WriteLine("5 - Kilograms to Pounds");
+            Console.WriteLine("6 - Pounds to Kilograms");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Test!");
+            Console.WriteLine("*** Unit converter ***");
+            printMenu();
+            string oper = Console.ReadLine();
+            string input;
+            double num;
+            switch(oper)
+            {
+                case "1":
+                    Console.WriteLine("Please enter the value to convert: ");
+                    input = Console.ReadLine();
+                    if (!Double.TryParse(input, out num))
+                    {
+                        Console.WriteLine("Invalid value. Please try again.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num} C equals to {(num * 1.8) + 32} F");
+                    }
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                default:
+                    break;
+
+            }
         }
     }
 }
