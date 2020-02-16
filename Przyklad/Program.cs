019f933faee8c24746719726a67642a97bf96f72
a_lachowicz <a_lachowicz@WWSI-0408>
2020-02-16 14:58:24 +0100

    przyklad

---
+++ Przyklad/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przyklad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        //Hello word
+        }
+
+    }
+}
