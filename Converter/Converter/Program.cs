8d4b355e1d442a54d1372a279c68bcf90dc1aaf4
Bjed123 <bar.jedrzynski@gmail.com>
2020-02-16 14:49:22 +0100

    1 Wersja konwertera

---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wybierz Kownerter \n 1 - temperatury \n 2 - długości \n 3 - masy");
+        }
+    }
+}
