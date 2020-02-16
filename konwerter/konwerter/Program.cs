7492c773c55e91471885376b767f294de51d62f2
l_kolodziejczyk <l_kolodziejczyk@WWSI-0423>
2020-02-16 10:37:59 +0100

    Łukasz Kołodziejczyk - Zadanie 1 - Konwerter

---
+++ konwerter/konwerter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace konwerter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
