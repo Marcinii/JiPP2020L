723281451eae9082bd166b9067828fd4bf9824ab
jmichalska55 <mrowkojad55@gmail.com>
2020-02-16 16:14:07 +0100

    Initial commit

---
+++ UnitValues.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konwerter
+{
+    public class UnitValues
+    {
+        public const double fromCtoF = 33.8;
+    }
+}
