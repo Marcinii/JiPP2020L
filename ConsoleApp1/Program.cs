ae5f63cfe7b8ec252704414132177af131074eb0
dabuv <dawidburzykowski@gmail.com>
2020-02-16 11:13:43 +0100

    Aktu 1.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! - zmiana");
+            double temp;
+            Console.WriteLine("Wybierz rodzaj konwersji:");
+            Console.WriteLine("1 - Celcius do Fahrenheit");
+            Console.WriteLine("2 - Fahrenheit do Celcius");
+            int pickle = Convert.ToInt32(Console.ReadLine());
+            switch (pickle)
+            {
+                case 1:
+                    Console.WriteLine("Wpisz wartość:");
+                    temp = Console.Read();
+                    Console.WriteLine("Temperatura Fahrenheit: " + (temp * (9 / 5) + 32) + "°F");
+                    break;
+                case 2:
+                    Console.WriteLine("Wpisz wartość:");
+                    temp = Console.Read();
+                    Console.WriteLine("Temperatura Fahrenheit: " + ((temp-32)/(9/5)) + "°F");
+                    break;
+             
+            }
         }
     }
 }
