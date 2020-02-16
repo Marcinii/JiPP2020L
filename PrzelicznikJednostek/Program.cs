3f706aa9f953afa28aca749ae0cfafb249004e0b
inutx <mateusz.jaron96@gmail.com>
2020-02-16 16:50:31 +0100

    Dodano opcje wyboru

--- PrzelicznikJednostek/Program.cs
+++ PrzelicznikJednostek/Program.cs
@@ -10,7 +10,41 @@ namespace PrzelicznikJednostek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("Wybierz jednostki: \n");
+            Console.WriteLine("1 - Temperatura - C -> F \n" +
+                "2 - Temperatura - F -> C \n" +
+                "3 - Dlugosc - km -> mile \n" +
+                "4 - Dlugosc - mile -> km \n" +
+                "5 - Waga - kg -> funty \n" +
+                "6 - Waga - funty -> kg \n");
+
+            string wybor;
+
+            wybor = Console.ReadLine();
+            switch (wybor)
+            {
+                case "1":
+                    
+                    break;
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+
+                case "5":
+
+                    break;
+
+                case "6":
+
+                    break;
+            }
         }
     }
 }
