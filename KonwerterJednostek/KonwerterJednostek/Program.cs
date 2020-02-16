c6b2d1231ef96216a5d9ca6dd14c2ffd942ec89b
j_zygmunt <j_zygmunt@poczta.wwsi.edu.pl>
2020-02-16 10:49:22 +0100

    Jakub Zygmunt - zadanie 1, commit początkowy

---
+++ KonwerterJednostek/KonwerterJednostek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KonwerterJednostek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w konwerterze jednostek!");
+            Console.WriteLine();
+            Console.WriteLine("Co chcesz zrobic?");
+            Console.WriteLine("\t(1) Konwersja temperatury");
+            Console.WriteLine("\t(2) Konwersja dlugosci");
+            Console.WriteLine("\t(3) Konwersja masy");
+            Console.WriteLine();
+            int choice;
+            choice = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
