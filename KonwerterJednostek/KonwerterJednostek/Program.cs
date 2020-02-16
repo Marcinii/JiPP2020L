91fc9050a9a24e4fa0da094dfebf40be6ed93373
j_zygmunt <j_zygmunt@poczta.wwsi.edu.pl>
2020-02-16 11:15:28 +0100

    Jakub Zygmunt - zad1, szkielet

--- KonwerterJednostek/KonwerterJednostek/Program.cs
+++ KonwerterJednostek/KonwerterJednostek/Program.cs
@@ -13,9 +13,37 @@ namespace KonwerterJednostek
             Console.WriteLine("\t(2) Konwersja dlugosci");
             Console.WriteLine("\t(3) Konwersja masy");
             Console.WriteLine();
-            int choice;
-            choice = Convert.ToInt32(Console.ReadLine());
-            //dowolna zmiaana
+            
+            int ch1, ch2;
+            ch1 = Convert.ToInt32(Console.ReadLine());
+
+            switch (ch1)
+            {
+                case 1:
+                    Console.WriteLine("KONWERSJA TEMPERATURY");
+                    Console.WriteLine("\t(1) Celsjusz\t->\tFahrenheit");
+                    Console.WriteLine("\t(2) Fahrenheit\t->\tCelsjusz");
+                    Console.WriteLine();
+                    //KOD
+                    break;
+                case 2:
+                    Console.WriteLine("KONWERSJA DLUGOSCI");
+                    Console.WriteLine("\t(1) Kilometry\t->\tMile");
+                    Console.WriteLine("\t(2) Mile\t->\tKilometry");
+                    Console.WriteLine();
+                    //KOD
+                    break;
+                case 3:
+                    Console.WriteLine("KONWERSJA MASY");
+                    Console.WriteLine("\t(1) Kilogramy\t->\tFunty");
+                    Console.WriteLine("\t(2) Funty\t->\tKilogramy");
+                    Console.WriteLine();
+                    //KOD
+                    break;
+                default:
+                    Console.WriteLine("!!! ERROR !!!");
+                    break;
+            }
         }
     }
 }
