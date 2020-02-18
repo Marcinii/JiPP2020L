704a366bd027d65b73a8cc31f78089b03e3b0a8f
Michał Michałowski <m.michalowski@poczta.wwsi.edu.pl>
2020-02-18 20:49:16 +0100

    Klasa konwertujaca jednostki, program glowny

--- Konwerter jednostek/Konwerter/Program.cs
+++ Konwerter jednostek/Konwerter/Program.cs
@@ -10,8 +10,55 @@ namespace Konwerter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            double amount;
+
             Console.WriteLine("Konwerter jednostek");
+            Console.WriteLine("a - Stopnie Celsjusza na Fahrenheita");
+            Console.WriteLine("b - Stopnie Fahrenheita na Celsjusza");
+            Console.WriteLine("c - Kilometry na mile");
+            Console.WriteLine("d - Mile na kilometry");
+            Console.WriteLine("e - Kilogramy na funty");
+            Console.WriteLine("f - Funty na kilogramy");
+            Console.WriteLine("Nacisnij odpowiedni klawisz...");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.A:
+                    Console.WriteLine("Wprowadz liczbe stopni Celsjusza:");
+                    amount = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(amount + " C to " + Math.Round(UnitConvertion.celToFahr(amount)) + " F");
+                    break;
+                case ConsoleKey.B:
+                    Console.WriteLine("Wprowadz liczbe stopni Fahrenheita:");
+                    amount = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(amount + " F to " + Math.Round(UnitConvertion.fahrToCel(amount)) + " C");
+                    break;
+                case ConsoleKey.C:
+                    Console.WriteLine("Wprowadz liczbe kilometrow:");
+                    amount = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(amount + " km to " + Math.Round(UnitConvertion.kmToMile(amount)) + " m");
+                    break;
+                case ConsoleKey.D:
+                    Console.WriteLine("Wprowadz liczbe mil:");
+                    amount = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(amount + " m to " + Math.Round(UnitConvertion.mileToKm(amount)) + " km");
+                    break;
+                case ConsoleKey.E:
+                    Console.WriteLine("Wprowadz liczbe kilogramow:");
+                    amount = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(amount + " kg to " + Math.Round(UnitConvertion.kgToPound(amount)) + " ibs");
+                    break;
+                case ConsoleKey.F:
+                    Console.WriteLine("Wprowadz liczbe funtow:");
+                    amount = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(amount + " ibs to " + Math.Round(UnitConvertion.poundToKg(amount)) + " kg");
+                    break;
+                default:
+                    Console.WriteLine("Wprowadzono zla opcje:");
+                    break;
+            }
+            Console.WriteLine("Wcisnij dowolny klawisz, aby zakonczyc...");
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Konwerter jednostek/Konwerter/UnitConvertion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwerter
+{
+    class UnitConvertion
+    {
+        public static double celToFahr(double cel)
+        {
+            return (2 * (cel - 0.1*cel)) + 32;
+        }
+        public static double fahrToCel(double fahr)
+        {
+            return ((fahr - 32) / 2) * 1.1;
+        }
+        public static double kmToMile(double km)
+        {
+            return km / 1.609;
+        }
+        public static double mileToKm(double mile)
+        {
+            return mile * 1.609;
+        }
+        public static double poundToKg(double pound)
+        {
+            return pound * 0.4535;
+        }
+        public static double kgToPound(double kg)
+        {
+            return kg / 0.4535;
+        }
+    }
+}
