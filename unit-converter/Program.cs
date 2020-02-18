a5e864889da1e338afafaf763e0ae5c1c29396e9
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2020-02-18 20:10:42 +0100

    Minor changes in Main.

--- unit-converter/Program.cs
+++ unit-converter/Program.cs
@@ -16,43 +16,55 @@ namespace unit_converter
             Console.WriteLine("4 - Miles to Kilometers");
             Console.WriteLine("5 - Kilograms to Pounds");
             Console.WriteLine("6 - Pounds to Kilograms");
+            Console.WriteLine("7 - Exit");
         }
         static void Main(string[] args)
         {
             Console.WriteLine("*** Unit converter ***");
             Converter converter = new Converter();
             printMenu();
-            string oper = Console.ReadLine();
-            
-            switch(oper)
+
+            bool programRunning = true;
+            string oper;
+
+            while (programRunning)
             {
-                case "1":
-                    Console.WriteLine("*** Converting Celsius to Farenheit ***");
-                    converter.celsiusToFahrenheit();
-                    break;
-                case "2":
-                    Console.WriteLine("*** Converting Farenheit to Celsius ***");
-                    converter.fahrenheitToCelsius();
-                    break;
-                case "3":
-                    Console.WriteLine("*** Converting Kilometers to Miles ***");
-                    converter.kilometersToMiles();
-                    break;
-                case "4":
-                    Console.WriteLine("*** Converting Miles to Kilometers ***");
-                    converter.milesToKilometers();
-                    break;
-                case "5":
-                    Console.WriteLine("*** Converting Kilograms to Pounds ***");
-                    converter.kilogramsToPounds();
-                    break;
-                case "6":
-                    Console.WriteLine("*** Converting Pounds to Kilograms ***");
-                    converter.poundsToKilograms();
-                    break;
-                default:
-                    Console.WriteLine("*** Invalid selection ***");
-                    break;
+                Console.WriteLine("Please enter your selection: ");
+                oper = Console.ReadLine();
+                switch (oper)
+                {
+                    case "1":
+                        Console.WriteLine("*** Converting Celsius to Farenheit ***");
+                        converter.celsiusToFahrenheit();
+                        break;
+                    case "2":
+                        Console.WriteLine("*** Converting Farenheit to Celsius ***");
+                        converter.fahrenheitToCelsius();
+                        break;
+                    case "3":
+                        Console.WriteLine("*** Converting Kilometers to Miles ***");
+                        converter.kilometersToMiles();
+                        break;
+                    case "4":
+                        Console.WriteLine("*** Converting Miles to Kilometers ***");
+                        converter.milesToKilometers();
+                        break;
+                    case "5":
+                        Console.WriteLine("*** Converting Kilograms to Pounds ***");
+                        converter.kilogramsToPounds();
+                        break;
+                    case "6":
+                        Console.WriteLine("*** Converting Pounds to Kilograms ***");
+                        converter.poundsToKilograms();
+                        break;
+                    case "7":
+                        Console.WriteLine("*** Exiting the program ***");
+                        programRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("*** Invalid selection ***");
+                        break;
+                }
             }
         }
     }
