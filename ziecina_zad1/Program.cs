214f4f0f5d8f5caec6d31a91f0b595e2418547f7
AzerimX <cezary.ziecina@gmail.com>
2020-02-23 13:58:10 +0100

    test1

--- ziecina_zad1/Program.cs
+++ ziecina_zad1/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ziecina_zad1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
-            
+            Console.WriteLine("Hello World!");
         }
     }
 }
