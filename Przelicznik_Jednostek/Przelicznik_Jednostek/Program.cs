3b7c07d948852f15c6f0b202f9edad23e747fa07
Bartek Cacek <Bartek.Cacek@gmail.com>
2020-02-25 21:16:01 +0100

    Zadanie 1 Cacek Bartłomiej. Gotowe rozwiązanie zadania 1, przeliczanie podstawowych jedostek.

--- Przelicznik_Jednostek/Przelicznik_Jednostek/Program.cs
+++ Przelicznik_Jednostek/Przelicznik_Jednostek/Program.cs
@@ -6,11 +6,63 @@ using System.Threading.Tasks;
 
 namespace Przelicznik_Jednostek
 {
-    class Program
+    class Program:operacje_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Przeliczanie jednostek, pierwszy projekt.")
+            Console.WriteLine("Przeliczanie jednostek, pierwszy projekt.");
+                int metoda = 0;
+            operacje_1 pyt_dzialanie = new operacje_1();
+
+            while (metoda != 4)
+
+            {
+                Console.WriteLine();
+                Console.WriteLine("Prosze wybrac dzialanie: ");
+                Console.WriteLine("Dla przeliczania stopni Celcjusz i Fahrenheita wybierz: '1' ");
+                Console.WriteLine("Dla przeliczania odleglosci Kilometrow i MIL wybierz: '2' ");
+                Console.WriteLine("Dla przeliczania wagi Kilogram i Funt wybierz: '3' ");
+                Console.WriteLine("Jeżeli chcesz zakonczyc program wybeirz: '4'  ");
+                Console.WriteLine();
+                metoda = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                switch (metoda)
+                {
+                    case 1: // przeliczanie celcjuszy i farenhtie
+
+                        pyt_dzialanie.C_czy_F();
+
+                        break;
+
+
+                    case 2: // przeliczanie mil i km
+
+                        pyt_dzialanie.KM_czy_M();
+
+                        break;
+
+
+                    case 3: // przeliczanie kg i funtów
+
+                        pyt_dzialanie.KG_czy_LBS();
+
+                        break;
+
+                    case 4: // zamykanie pętli
+
+                         metoda = 4;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Nie wybrano zadania!");
+                        Console.WriteLine("Sprobuj jeszcze raz.");
+                        Console.WriteLine();
+                        break;
+                }
+
+            } 
+
         }
     }
 }
---
+++ Przelicznik_Jednostek/Przelicznik_Jednostek/operacje_1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przelicznik_Jednostek
+{
+    public class operacje_1 : Dzialania
+    {
+        protected double c, f, km, m, kg, ibs;
+        Dzialania operacja = new Dzialania();
+        public void C_czy_F()
+        {
+            // pytanie o sposób przeliczania z jakich jednostek stopni na jakie z Celcjuszy czy z Farenhita?
+            // równierz pętla aby można było zrobić kilka przeliczeń
+
+            int cf = 0;
+            
+           
+            while (cf != 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Przeliczanie z Celcjusza na Fahrenheite wybierz: '1'");
+                Console.WriteLine("Przeliczanie z Farenhite na Celcjusza wybierz: '2'");
+                Console.WriteLine("Zakoncz przeliczanie stopni i przejdz do wyboru zadan: '3'");
+                Console.WriteLine();
+                cf = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                switch (cf)
+                {
+                    case 1: // z celcjuszy na farenhite
+                        Console.WriteLine();
+                        Console.WriteLine("Podaj temperature w stopniach Celcjusza: ");
+                        // odesłanie do funkcji która tylko liczy
+                        c = double.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine($"Temperatura {c} stopni Celcjusza.");
+                        operacja.celcjusz(c);
+                        Console.WriteLine();
+
+                        break;
+
+                    case 2: // z farenhite na celcjusze
+                        Console.WriteLine();
+                        Console.WriteLine("Podaj temperature w stopniach Fahrenheita: ");
+                        // odesłanie do funkcji która tylko liczy
+                        f = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Temperatura {f} stopni Fahrenheita.");
+                        operacja.fahrenheit(f);
+                        Console.WriteLine();
+                        break;
+
+                    case 3: // zakończ działanie
+                      
+                        break;
+
+                    default: // powtórz wybór
+                        Console.WriteLine();
+                        Console.WriteLine("Nie wybrano dzialania!");
+                        Console.WriteLine("Sprobuj jeszcze raz.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+
+       public void KM_czy_M()
+        {
+            // pytanie o sposób przeliczania z jakich jednostek odległości na jakie z kilometrów czy z mil?
+            // równierz pętla aby można było zrobić kilka przeliczeń
+
+            int k_m = 0;
+            while (k_m != 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Przeliczanie z Kilometrow na Mile wybierz: '1'");
+                Console.WriteLine("Przeliczanie z Mil na Kilometry wybierz: '2'");
+                Console.WriteLine("Zakoncz przeliczanie odleglosci i przejdz do wyboru zadan: '3'");
+                Console.WriteLine();
+                k_m = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                switch (k_m)
+                {
+                    case 1: // z KM na M
+                        Console.WriteLine();
+                        Console.WriteLine("Podaj odleglosc w Kilometrach: ");
+                        // odesłanie do funkcji która tylko liczy
+                        km = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Odleglosc {km} w Kilometrach.");
+                        operacja.kilometry(km);
+                        Console.WriteLine();
+                        break;
+
+                    case 2: // z M na KM
+                        Console.WriteLine();
+                        Console.WriteLine("Podaj odleglosc w Milach: ");
+                        // odesłanie do funkcji która tylko liczy
+                        m = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Odleglosc {m} w Milach.");
+                        operacja.mile(m);
+                        Console.WriteLine();
+                        break;
+
+                    case 3: // zakończ działanie
+
+                        break;
+
+                    default: // powtórz wybór
+                        Console.WriteLine();
+                        Console.WriteLine("Nie wybrano dzialania!");
+                        Console.WriteLine("Sprobuj jeszcze raz.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+
+       public  void KG_czy_LBS()
+        {
+            // pytanie o sposób przeliczania z jakich jednostek wagi na jakie z Kilogramów czy z Funtów?
+            // równierz pętla aby można było zrobić kilka przeliczeń
+
+            int kg_lbs = 0;
+            while (kg_lbs != 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Przeliczanie z Kilogram na Funt wybierz: '1'");
+                Console.WriteLine("Przeliczanie z Funt na Kilogram wybierz: '2'");
+                Console.WriteLine("Zakoncz przeliczanie stopni i przejdz do wyboru zadan: '3'");
+                Console.WriteLine();
+                kg_lbs = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                switch (kg_lbs)
+                {
+                    case 1: // z kg na lbs
+                        Console.WriteLine();
+                        Console.WriteLine("Podaj wage w Kilogramach: ");
+
+                        // odesłanie do funkcji która tylko liczy
+                        kg = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Waga {kg} w Kilogramach.");
+                        operacja.kilogramy(kg);
+                        Console.WriteLine();
+                        break;
+
+                    case 2: // z lbs na kg
+                        Console.WriteLine();
+                        Console.WriteLine("Podaj wage w Funtach: ");
+                        // odesłanie do funkcji która tylko liczy
+                        ibs = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"Waga {ibs} w Funtach.");
+                        operacja.funty(ibs);
+                        Console.WriteLine();
+                        break;
+
+                    case 3: // zakończ działanie
+
+                        break;
+
+                    default: // powtórz wybór
+                        Console.WriteLine();
+                        Console.WriteLine("Nie wybrano dzialania!");
+                        Console.WriteLine("Sprobuj jeszcze raz.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+
+    }
+}
