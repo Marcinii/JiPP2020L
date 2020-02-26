4bc8e86f7d31b7991410ff7697d36ab34800efb3
Luka Lasocki <xlord@o2.pl>
2020-02-26 20:42:49 +0100

    Z401-Lukasz Lasocki - zadanie 1 - konwerter jednostek

---
+++ Konwerter_ver01/Konwerter_ver01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwerter_ver01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("Chcesz coś przekonwertować? (jeśli TAK wpisz t)");
+            char odp = char.Parse(Console.ReadLine());
+            if (odp == 't')
+            {
+                Konwerter tmp = new Konwerter();
+                tmp.numeropcji();
+                tmp.konwerterjednostek();
+                goto start;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
