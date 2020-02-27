54d155d3d27a19a18a1ea14ac13eea0d330a5cd5
Piotr Kowalczyk <p-kowalczyk@outlook.com>
2020-02-27 22:06:48 +0100

    initial commit

---
+++ Project/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[1] Zamiana stopni Celsjusza na Farenheita");
+            Console.WriteLine("[2] Zamiana stopni Farenheita na Celsjusza");
+            Console.WriteLine("[3] Zamiana km na mile");
+            Console.WriteLine("[4] Zamiana mil na km");
+            Console.WriteLine("[5] Zamiana kg na funty");
+            Console.WriteLine("[6] Zamiana funtow na kg");
+
+            Console.Write("Wybor: ");
+            int choise = Int32.Parse(Console.ReadLine());
+            Console.Write("Podaj wartosc: ");
+
+            int value = Int32.Parse(Console.ReadLine());
+
+            switch(choise)
+            {
+                case 1:
+                    Console.WriteLine("Wynik = " + Converter.Temperature.CToF(value));
+                    break;
+                case 2:
+                    Console.WriteLine("Wynik = " + Converter.Temperature.FToC(value));
+                    break;
+                case 3:
+                    Console.WriteLine("Wynik = " + Converter.Length.KmToMiles(value));
+                    break;
+                case 4:
+                    Console.WriteLine("Wynik = " + Converter.Length.MilesToKm(value));
+                    break;
+                case 5:
+                    Console.WriteLine("Wynik = " + Converter.Weight.KgToLb(value));
+                    break;
+                case 6:
+                    Console.WriteLine("Wynik = " + Converter.Weight.LbToKg(value));
+                    break;
+                default:
+                    Console.WriteLine("Niepoprawny wybor");
+                    break;            
+            }
+
+        }
+    }
+}
---
+++ Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Project")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7cb485b-3b5f-4a1b-9730-a6aa05d7dc0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
