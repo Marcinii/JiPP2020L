acd10c5b6a9a91527073bc314b88d2d433d2cbcc
o_andres <o_andres@poczta.wwsi.edu.pl>
2020-02-27 21:18:34 +0100

    Zadanie 1 Napisanie przelicznika jednostek.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Wybierz jednostki: ");
+            Console.WriteLine("1.Temperatury (Celcjusz, Farenheit \n2.Długości (Kilometry, Mile) \n3.Masy (Kilogramy, Funty)");
+            
+            string symbol1 = Console.ReadLine();
+            switch (symbol1)
+            {
+                case "1":
+                    Console.WriteLine("1.Temperatury (Celcjusz, Farenheit) \n Wybierz: \n 1.Celcjusz na Farenheit \n 2.Farenheit na Celcjusz");
+                    string symbol2 = Console.ReadLine();
+                    Console.WriteLine("Podaj liczbę: ");
+                    var jednostka = Console.ReadLine();
+                    var value = Convert.ToInt32(jednostka);
+                    switch (symbol2)
+                    {
+                        case "1":
+                            Console.WriteLine(value + " C to " + Przelicznik.liczTemperature(value, symbol2)+" F");
+                            break;
+                        case "2":
+                            Console.WriteLine(value + " F to " + Przelicznik.liczTemperature(value, symbol2)+" C");
+                            break;
+                        default:
+                            Console.WriteLine("Podaj poprawną wartość!");
+                                break;
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("2.Długości (Kilometry, Mile) \n Wybierz: \n 1.Kilometry na Mile \n 2.Mile na Kilometry");
+                    string symbol3 = Console.ReadLine();
+                    Console.WriteLine("Podaj liczbę: ");
+                    var jednostka2 = Console.ReadLine();
+                    var value2 = Convert.ToInt32(jednostka2);
+                    switch (symbol3)
+                    {
+                        case "1":
+                            Console.WriteLine(value2 + " km to " + Przelicznik.liczDlugosci(value2, symbol3)+" Mm");
+                            break;
+                        case "2":
+                            Console.WriteLine(value2 + " Mm to " + Przelicznik.liczDlugosci(value2, symbol3)+" km");
+                            break;
+                        default:
+                            Console.WriteLine("Podaj poprawną wartość!");
+                            break;
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("3.Masy (Kilogramy, Funty) \n Wybierz: \n 1.Kilogramy na Funty \n 2.Funty na Kilogramy");
+                    string symbol4 = Console.ReadLine();
+                    Console.WriteLine("Podaj liczbę: ");
+                    var jednostka3 = Console.ReadLine();
+                    var value3 = Convert.ToInt32(jednostka3);
+                    switch (symbol4)
+                    {
+                        case "1":
+                            Console.WriteLine(value3 + " kg to " + Przelicznik.liczMase(value3, symbol4)+" lb");
+                            break;
+                        case "2":
+                            Console.WriteLine(value3 + " lb to " + Przelicznik.liczMase(value3, symbol4)+" kg");
+                            break;
+                        default:
+                            Console.WriteLine("Podaj poprawną wartość!");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Przelicznik.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+   public class Przelicznik {
+       
+       public static double liczTemperature(double jednostka, string strona)
+        {
+            double result;
+            if(strona == "1") // C->F
+            {
+                result = jednostka * 1.8000 + 32.00;
+                return result;
+            }
+            else if (strona == "2") // F->C
+            {
+                result = (jednostka - 32.00) / 1.8000;
+               return result;
+            }
+            else
+            {
+                Console.WriteLine("Podaj odpowiednia liczbe!");
+                return 0;
+            }
+        }
+        public static double liczDlugosci(double jednostka, string strona)
+        {
+            double result;
+            if (strona == "1") // K->M
+            {
+                result = jednostka * 0.62137;
+                return result;
+            }
+            else if (strona == "2") // M->K
+            {
+                result = jednostka / 0.62137;
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Podaj odpowiednia liczbe!");
+                return 0;
+            }
+        }
+        public static double liczMase(double jednostka, string strona)
+        {
+            double result;
+            if (strona == "1") // K->F
+            {
+                result = jednostka * 2.2046;
+                return result;
+            }
+            else if (strona == "2") // F->K
+            {
+                result = jednostka / 2.2046;
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Podaj odpowiednia liczbe!");
+                return 0;
+            }
+        }
+    }
+}
