cec6500be9bf299be5bcc74f4401a6f69ae49c64
p_waryszewski@poczta.wwsi.edu.pl <p_waryszewski@poczta.wwsi.edu.pl>
2020-02-29 19:38:28 +0100

    Z404 Piotr Waryszewski zadanie 1 konweeter

---
+++ Zadanie 1 konwerter/Zadanie 1konwerter/Konwerter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZAD_1
+{
+    class Konwerter
+    {
+        double dana, wynik;
+        public Konwerter() { }
+        public void konwertuj_na_C(double dana)
+        {
+            this.dana = dana;
+            wynik = (dana - 32) / 1.8;
+            Console.WriteLine(this.dana.ToString() + "F" + " = " + wynik.ToString() + "C");
+        }
+        public void konwertuj_na_F(double dana)
+        {
+            this.dana = dana;
+            wynik = (dana * 1.8) + 32;
+            Console.WriteLine(this.dana.ToString() + "C" + " = " + wynik.ToString() + "F");
+        }
+        public void konwertuj_na_Funty(double dana)
+        {
+            this.dana = dana;
+            wynik = dana * 2.2046;
+            Console.WriteLine(this.dana.ToString() + "Kg" + " = " + wynik.ToString() + "Funta");
+        }
+        public void konwertuj_na_Kg(double dana)
+        {
+            this.dana = dana;
+            wynik = dana / 2.2046;
+            Console.WriteLine(this.dana.ToString() + "Funta" + " = " + wynik.ToString() + "Kg");
+        }
+        public void konwertuj_na_Mile(double dana)
+        {
+            this.dana = dana;
+            wynik = dana * 0.62137;
+            Console.WriteLine(this.dana.ToString() + "Mil" + " = " + wynik.ToString() + "Km");
+        }
+        public void konwertuj_na_Kilometry(double dana)
+        {
+            this.dana = dana;
+            wynik = dana / 0.62137;
+            Console.WriteLine(this.dana.ToString() + "Km" + " = " + wynik.ToString() + "Mil");
+        }
+    }
+}
---
+++ Zadanie 1 konwerter/Zadanie 1konwerter/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ZAD_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dana;
+            int wybor;
+            Konwerter konwerter = new Konwerter();
+            Console.WriteLine("KONWERTER JEDNOSTEK" +
+            "\n Wybierz co chcesz zrobić (1 - 6)" +
+            "\n 1. Konwersja stopni (F na C)" +
+            "\n 2. Konwersja stopni (C na F)" +
+            "\n 3. Konwersja masy (Kilogramy na Funty)" +
+            "\n 4. Konwersja masy (Funty na Kilogramy)" +
+            "\n 5. Konwersja odległości (Kilometry na Mile)" +
+            "\n 6. Konwersja odległości (Mile na Kilometry)");
+            wybor = int.Parse(Console.ReadLine());
+            switch (wybor)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Podaj wartość do przeliczenia: ");
+                            dana = double.Parse(Console.ReadLine());
+                            konwerter.konwertuj_na_C(dana);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Podaj wartość do przeliczenia: ");
+                            dana = double.Parse(Console.ReadLine());
+                            konwerter.konwertuj_na_F(dana);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Podaj wartość do przeliczenia: ");
+                            dana = double.Parse(Console.ReadLine());
+                            konwerter.konwertuj_na_Funty(dana);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Podaj wartość do przeliczenia: ");
+                            dana = double.Parse(Console.ReadLine());
+                            konwerter.konwertuj_na_Kg(dana);
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Podaj wartość do przeliczenia: ");
+                            dana = double.Parse(Console.ReadLine());
+                            while (dana < 0)
+                            {
+                                Console.WriteLine("Podaj wartość większą od zera:");
+                                dana = double.Parse(Console.ReadLine());
+                            }
+                            konwerter.konwertuj_na_Mile(dana);
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Podaj wartość do przeliczenia: ");
+                            dana = double.Parse(Console.ReadLine());
+                            while (dana < 0)
+                            {
+                                Console.WriteLine("Podaj wartość większą od zera:");
+                                dana = double.Parse(Console.ReadLine());
+                            }
+                            konwerter.konwertuj_na_Kilometry(dana);
+                            break;
+                        }
+            }            
+        }
+    }
+}
