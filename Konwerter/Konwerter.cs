92b0e734be7633a56d972b72ef7ff51e4a343f95
SSJ0406 <slawomirsawicki1995@gmail.com>
2020-03-01 14:17:26 +0100

    Sławomir Sawicki - zadanie 1, commit początkowy"

---
+++ Konwerter/Konwerter.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konwerter
+{
+    class Konwerter
+    {
+        protected double temperaturaWCelsjuszach;
+        protected double temperaturaWFarenheitach;
+        protected double dlugoscWKilometrach;
+        protected double dlugoscWMilach;
+        protected double wagaWKilogramach;
+        protected double wagaWFuntach;
+
+        public double CelsjuszeNaFarenheity(double temperatura)
+        {
+            temperaturaWCelsjuszach = temperatura;
+            Console.WriteLine("Temperatura w Celsjuszach: " + temperaturaWCelsjuszach.ToString());
+            temperaturaWFarenheitach = ((9 / 5) * temperaturaWCelsjuszach) + 32;
+            return temperaturaWFarenheitach;
+        }
+
+        public double AktualnaTemperaturawFarenheitach()
+        {
+            return temperaturaWFarenheitach;
+        }
+
+        public void PokazTemperatureWFarenheitach()
+        {
+            double tmpWF = AktualnaTemperaturawFarenheitach();
+            Console.WriteLine("Temperatura w Farenheitach: " + tmpWF.ToString());
+        }
+
+        public double FarenheityNaCelsjusze(double temperatura)
+        {
+            temperaturaWFarenheitach = temperatura;
+            Console.WriteLine("Temperatura w Farenheitach: " + temperaturaWFarenheitach.ToString());
+            temperaturaWCelsjuszach = (temperaturaWFarenheitach - 32) / (9/5);
+            return temperaturaWCelsjuszach;
+        }
+
+        public double AktualnaTemperaturawCelsjuszach()
+        {
+            return temperaturaWCelsjuszach;
+        }
+
+        public void PokazTemperatureWCelsjuszach()
+        {
+            double tmpC = AktualnaTemperaturawCelsjuszach();
+            Console.WriteLine("Temperatura w Celsjuszach: " + tmpC.ToString());
+        }
+
+        public double KilometryNaMile(double km)
+        {
+            dlugoscWKilometrach = km;
+            Console.WriteLine("Dlugosc w kilometrach: " + dlugoscWKilometrach.ToString());
+            dlugoscWMilach = dlugoscWKilometrach*1.609;
+            return dlugoscWMilach;
+        }
+
+        public double AktualnaDlugoscWMilach()
+        {
+            return dlugoscWMilach;
+        }
+
+        public void PokazDlugoscWMilach()
+        {
+            double dlWMilach = AktualnaDlugoscWMilach();
+            Console.WriteLine("Dlugosc w Milach: " + dlWMilach.ToString());
+        }
+
+        public double MileNaKilometry(double mile)
+        {
+            dlugoscWMilach = mile;
+            Console.WriteLine("Dlugosc w milach: " + dlugoscWMilach.ToString());
+            dlugoscWKilometrach = dlugoscWMilach * 0.621371192;
+            return dlugoscWKilometrach;
+        }
+
+        public double AktualnaDlugoscWKilometrach()
+        {
+            return dlugoscWKilometrach;
+        }
+
+        public void PokazDlugoscWKilometrach()
+        {
+            double dlWKM = AktualnaDlugoscWKilometrach();
+            Console.WriteLine("Dlugosc w Kilometrach: " + dlWKM.ToString());
+        }
+
+        public double KilogramyNaFunty(double kg)
+        {
+            wagaWKilogramach = kg;
+            Console.WriteLine("Waga w kilogramach: " + wagaWKilogramach.ToString());
+            wagaWFuntach = wagaWKilogramach * 2.20462262;
+            return wagaWFuntach;
+        }
+
+        public double AktualnaWagaWFuntach()
+        {
+            return wagaWFuntach;
+        }
+
+        public void PokazWageWFuntach()
+        {
+            double wagaWFuntach = AktualnaWagaWFuntach();
+            Console.WriteLine("Waga w funtach: " + wagaWFuntach.ToString());
+        }
+
+        public double FuntyNaKilogramy(double funty)
+        {
+            wagaWFuntach = funty;
+            Console.WriteLine("Waga w funtach: " + wagaWFuntach.ToString());
+            wagaWKilogramach = wagaWFuntach * 0.45359237;
+            return wagaWKilogramach;
+        }
+
+        public double AktualnaWagaWKilogramach()
+        {
+            return wagaWKilogramach;
+        }
+
+        public void PokazWageWKilogramach()
+        {
+            double wagaWKG = AktualnaWagaWKilogramach();
+            Console.WriteLine("Waga w Kilogramach: " + wagaWKG.ToString());
+        }
+
+    }
+}
--- Konwerter/Program.cs
+++ Konwerter/Program.cs
@@ -4,110 +4,97 @@ namespace Konwerter
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Wpisz: \n 1 - konwerter temperatur \n 2 - konwerter dlugosci \n 3 - konwerter mas");
+            Konwerter konwerter = new Konwerter();
 
-            int a;
-
-            a = int.Parse(Console.ReadLine());
-
-            if (a == 1)
+            int wybor = 0, parametr = 0;
+            double tmp = 0, dl = 0, waga = 0;
+            Console.WriteLine("\r\n Wybierz konwerter: \r\n 1. Konwerter jednostek \r\n 2. Konwerter dlugosci: \r\n 3. Konwerter masy: ");
+            wybor = Convert.ToInt32(Console.ReadLine());
+            switch (wybor)
             {
-                ConvertTemp();
-            }
-            else if (a == 2)
-            {
-                ConvertLengths();
-            }
-            else if (a == 3)
-            {
-                ConvertMass();
-            }
-
-            void ConvertTemp() 
-            {
-                Console.WriteLine("Wpisz: \n 1 - Konwertuj Celsjusze na Farenheity \n 2 - Konwertuj Farenheity na Celsjusze");
-
-                int b;
-                float temp;
-                float oldTemp;
-                
-                b = int.Parse(Console.ReadLine());
-
-                if (b == 1)
-                {
-                    Console.WriteLine("Wpisz temperature");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = (temp * 1.8f) + 32f;
-                    Console.WriteLine(oldTemp + " Celsjuszy to " + temp + " Farenheitow");
-                }
-                else if (b == 2)
-                {
-                    Console.WriteLine("Wpisz temperature");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = (temp - 32f) / 1.8f;
-                    Console.WriteLine(oldTemp + " Farenheitow to " + temp + " Celsjuszy");
-                }
-            }
-
-            void ConvertLengths() 
-            {
-                Console.WriteLine("Wpisz: \n 1 - Konwertuj kilometry na mile \n 2 - Konwertuj mile na kilometry");
-
-                int b;
-                float temp;
-                float oldTemp;
-
-                b = int.Parse(Console.ReadLine());
-
-                if (b == 1)
-                {
-                    Console.WriteLine("Wpisz dlugosc");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp * 0.62137f;
-                    Console.WriteLine(oldTemp + " kilometrow to " + temp + " mil");
-                }
-                else if (b == 2)
-                {
-                    Console.WriteLine("Wpisz dlugosc");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp / 0.62137f;
-                    Console.WriteLine(oldTemp + " mil to " + temp + " kilometrow");
-                }
-            }
-
-            void ConvertMass()
-            {
-                Console.WriteLine("Wpisz: \n 1 - Konwertuj kilogramy na funty \n 2 - Konwertuj funty na kilogramy");
-
-                int b;
-                float temp;
-                float oldTemp;
-
-                b = int.Parse(Console.ReadLine());
-
-                if (b == 1)
-                {
-                    Console.WriteLine("Wpisz mase");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp * 2.2046f;
-                    Console.WriteLine(oldTemp + " kilogramow to " + temp + " funtow");
-                }
-                else if (b == 2)
-                {
-                    Console.WriteLine("Wpisz dlugosc");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp / 2.2046f;
-                    Console.WriteLine(oldTemp + " funtow to " + temp + " kilogramy");
-                }
+                case 1:
+                    Console.WriteLine("Konwerter jednostek");
+                    Console.WriteLine("\r\n Wybierz zamiane: \r\n 1. Skala Celsjusza na Farenheita \r\n 2. Skala Farenheita na Celsjusza: \r\n");
+                    parametr = Convert.ToInt32(Console.ReadLine());
+                    switch (parametr)
+                    {
+                        case 1:
+                            Console.WriteLine("Skala Celsjusza na Farenheita");
+                            Console.WriteLine("Podaj temperature w Celsjuszach: ");
+                            tmp = Convert.ToDouble(Console.ReadLine());
+                            konwerter.CelsjuszeNaFarenheity(tmp);
+                            konwerter.PokazTemperatureWFarenheitach();
+                            break;
+                        case 2:
+                            Console.WriteLine("Skala Farenheita na Celsjusza");
+                            Console.WriteLine("Podaj temperature w Farenheitach: ");
+                            tmp = Convert.ToDouble(Console.ReadLine());
+                            konwerter.FarenheityNaCelsjusze(tmp);
+                            konwerter.PokazTemperatureWCelsjuszach();
+                            break;
+                        default:
+                            Console.WriteLine("Zly wybor");
+                            break;
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Konwerter dlugosci");
+                    Console.WriteLine("\r\n Wybierz zamiane: \r\n 1. Kilometry na mile \r\n 2. Mile na kilometry: \r\n");
+                    parametr = Convert.ToInt32(Console.ReadLine());
+                    switch (parametr)
+                    {
+                        case 1:
+                            Console.WriteLine("Zamiana kilometrow na mile");
+                            Console.WriteLine("Podaj dlugosc w kilometrach: ");
+                            dl = Convert.ToDouble(Console.ReadLine());
+                            konwerter.KilometryNaMile(dl);
+                            konwerter.PokazDlugoscWMilach();
+                            break;
+                        case 2:
+                            Console.WriteLine("Zamiana mil na kilometry");
+                            Console.WriteLine("Podaj dlugosc w milach: ");
+                            dl = Convert.ToDouble(Console.ReadLine());
+                            konwerter.MileNaKilometry(dl);
+                            konwerter.PokazDlugoscWKilometrach();
+                            break;
+                        default:
+                            Console.WriteLine("Zly wybor");
+                            break;
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Konwerter masy");
+                    Console.WriteLine("\r\n Wybierz zamiane: \r\n 1. Kilogramy na funty \r\n 2. Funty na kilogramy: \r\n");
+                    parametr = Convert.ToInt32(Console.ReadLine());
+                    switch (parametr)
+                    {
+                        case 1:
+                            Console.WriteLine("Zamiana kilogramow na funty");
+                            Console.WriteLine("Podaj wage w kilogramach: ");
+                            waga = Convert.ToDouble(Console.ReadLine());
+                            konwerter.KilogramyNaFunty(waga);
+                            konwerter.PokazWageWFuntach();
+                            break;
+                        case 2:
+                            Console.WriteLine("Zamiana funtow na kilogramy");
+                            Console.WriteLine("Podaj wage w funtach: ");
+                            waga = Convert.ToDouble(Console.ReadLine());
+                            konwerter.FuntyNaKilogramy(waga);
+                            konwerter.PokazWageWKilogramach();
+                            break;
+                        default:
+                            Console.WriteLine("Zly wybor");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Zly wybor");
+                    break;
             }
         }
     }
 }
+
