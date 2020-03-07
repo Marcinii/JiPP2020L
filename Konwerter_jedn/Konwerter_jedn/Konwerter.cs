48da29ba3bfb8638005ff21217ba0409f87e1759
Kasia Modzelewska <katarzyna@modzelewska.eu>
2020-03-07 19:22:18 +0100

    Konwerter-wersja2

---
+++ Konwerter_jedn/Konwerter_jedn/Konwerter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwerter_jedn
+{
+    public class Konwerter
+    {
+        public void CtoF()
+        {
+            double F, C;
+            Console.WriteLine("Podaj temperature w C");
+            C=int.Parse(Console.ReadLine());
+            F = (C*1.8f)+32f;
+            Console.WriteLine("To jest " + F + " stopni Fahrenheita");
+        }
+
+        public void FtoC()
+        {
+            double F, C;
+            Console.WriteLine("Podaj temperature w F");
+            F=int.Parse(Console.ReadLine());
+            C = (F-32f)/1.8f;
+            Console.WriteLine("To jest " + C + " stopni Celsjusza");
+        }
+
+        public void KmtoM()
+        {
+            double Km = 0, M = 0;
+            Console.WriteLine("Podaj odleglosc w kilometrach");
+            Km=int.Parse(Console.ReadLine());
+            M=Km/1.609344;
+            Console.WriteLine("To jest " + M + " mil");
+        }
+
+        public void MtoKm()
+        {
+            double M = 0, Km = 0;
+            Console.WriteLine("Podaj odleglosc w milach");
+            M=int.Parse(Console.ReadLine());
+            Km=M*1.609344;
+            Console.WriteLine("To jest " + Km + " kilometrow");
+        }
+
+        public void KgtoF()
+        {
+            double Kg = 0, F = 0;
+            Console.WriteLine("Podaj wage w kilogramach");
+            Kg=int.Parse(Console.ReadLine());
+            F=Kg*2.2046f;
+            Console.WriteLine("To jest " + F + " funtow");
+        }
+
+        public void FtoKg()
+        {
+            double Kg = 0, F = 0;
+            Console.WriteLine("Podaj wage w funtach");
+            F=int.Parse(Console.ReadLine());
+            Kg= F/2.2046f;
+            Console.WriteLine("To jest " + Kg + " kilogramow");
+        }
+    }
+}
--- Konwerter_jedn/Konwerter_jedn/Program.cs
+++ Konwerter_jedn/Konwerter_jedn/Program.cs
@@ -8,8 +8,45 @@ namespace Konwerter_jedn
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int wybor = 0;
+            Konwerter nowy = new Konwerter();
+            Console.WriteLine("To konwerter jednostek. Wybierz operacje.");
+            Console.WriteLine("Jednostki temperatury:");
+            Console.WriteLine("1. Zamiana stopni Celsjusza na stopnie Fahrenheita;");
+            Console.WriteLine("2. Zamiana stopni Fahrenheita na stopnie Celsjusza;");
+            Console.WriteLine("Jednostki odleglosci:");
+            Console.WriteLine("3. Zamiana kilometrow na mile;");
+            Console.WriteLine("4. Zamiana mil na kilometry;");
+            Console.WriteLine("Jednostki masy:");
+            Console.WriteLine("5. Zamiana kilogramow na funty;");
+            Console.WriteLine("6. Zamiana funtow na kilogramy;");
+            wybor = Convert.ToInt32(Console.ReadLine());
+            switch(wybor)
+            {
+                case 1:
+                    nowy.CtoF();
+                    break;
+                case 2:
+                    nowy.FtoC();
+                    break;
+                case 3:
+                    nowy.KmtoM();
+                    break;
+                case 4:
+                    nowy.MtoKm();
+                    break;
+                case 5:
+                    nowy.KgtoF();
+                    break;
+                case 6:
+                    nowy.FtoKg();
+                    break;
+
+
+            }
+            Console.ReadLine();
         }
     }
 }
