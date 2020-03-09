a8d31a7ed93c5b57650b56d87d24846c9b80b1a7
Wojciech Kępka <wojtek.kepka@protonmail.com>
2020-03-09 18:24:04 +0100

    Przeniesienie logiki do zewnętrznej biblioteki

--- konwerter_jednostek_Z412_Wojciech_Kepka/konwerter_jednostek_Z412_Wojciech_Kepka/Program.cs
+++ konwerter_jednostek_Z412_Wojciech_Kepka/konwerter_jednostek_Z412_Wojciech_Kepka/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static Converter.Units;
+using static Converter.Formulas;
 
 namespace UnitConverter
 {
@@ -248,59 +250,7 @@ namespace UnitConverter
             }
         }
         //################################################
-        // Temperatures
-        public static double CelsiusToFahrenheit(double inp) { return inp * (9 / 5) + 32; }
-        public static double CelsiusToKelvin(double inp ) { return inp + 273.15; }
-        public static double FahrenheitToCelsius(double inp) { return (inp - 32) * 5 / 9; }
-        public static double FahrenheitToKelvin(double inp) { return (inp + 459.67) * 5 / 9; }
-        public static double KelvinToCelsius(double inp) { return inp - 273.15; }
-        public static double KelvinToFahrenheit(double inp) { return (inp * 9 / 5) - 459.67; }
-        // Mass
-        public static double KilogramsToPounds(double inp) { return inp * 2.2046; }
-        public static double KilogramsToOunces(double inp) { return inp * 35.2739619496; }
-        public static double OuncesToPounds(double inp) { return inp * 0.0625; }
-        public static double OuncesToKilograms(double inp) { return inp * 0.0283495231; }
-        public static double PoundsToKilograms(double inp) { return inp / 2.2046; }
-        public static double PoundsToOunces(double inp) { return inp / 0.0625; }
-        // Distance
-        public static double KilometersToMiles(double inp) { return inp * 0.621371192; }
-        public static double MilesToKilometers(double inp) {  return inp / 0.621371192; }
-        //################################################
-        public enum Unit
-        {
-            Celsius,
-            Fahrenheit,
-            Kelvin,
-            Kilometers,
-            Miles,
-            Kilograms,
-            Pounds,
-            Ounces
-        }
-        public static string UnitName(Unit unit)
-        {
-            switch (unit)
-            {
-                case Unit.Celsius:
-                    return "C";
-                case Unit.Fahrenheit:
-                    return "F";
-                case Unit.Kelvin:
-                    return "K";
-                case Unit.Kilograms:
-                    return "kg";
-                case Unit.Pounds:
-                    return "lb";
-                case Unit.Ounces:
-                    return "oz";
-                case Unit.Kilometers:
-                    return "km";
-                case Unit.Miles:
-                    return "mi";
-                default:
-                    return "";
-            }
-        }
+
         static void Main(string[] args)
         {
             Converter conv = new Converter();
