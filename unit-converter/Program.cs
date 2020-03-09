0bda7590df75e6a79e36059d2fa9baf6b1a7190f
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2020-03-09 23:30:36 +0100

    Changes in Main.

--- unit-converter/Program.cs
+++ unit-converter/Program.cs
@@ -8,6 +8,43 @@ namespace unit_converter
 {
     class Program
     {
+        static void EnterData(IConverter converter)
+        {
+            Console.WriteLine("*** Available Units ***");
+            foreach (string unit in converter.AvailableUnits)
+            {
+                Console.WriteLine(unit);
+            }
+            Console.WriteLine("Please enter the source unit: ");
+            string sourceUnit = Console.ReadLine();
+            if (converter.AvailableUnits.IndexOf(sourceUnit) == -1)
+            {
+                Console.WriteLine("Invalid unit selected");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the target unit: ");
+                string targetUnit = Console.ReadLine();
+                if (converter.AvailableUnits.IndexOf(targetUnit) == -1)
+                {
+                    Console.WriteLine("Invalid unit selected");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the value to convert: ");
+                    string strValue = Console.ReadLine();
+                    if (!converter.IsInputValid(strValue))
+                    {
+                        Console.WriteLine("Invalid value");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Conversion result: {0}", converter.Convert(
+                            sourceUnit, targetUnit, double.Parse(strValue)));
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
             List<IConverter> availableConverters = new List<IConverter>()
@@ -36,15 +73,19 @@ namespace unit_converter
                 {
                     case "1":
                         selectedConverter = availableConverters[0];
+                        EnterData(selectedConverter);
                         break;
                     case "2":
                         selectedConverter = availableConverters[1];
+                        EnterData(selectedConverter);
                         break;
                     case "3":
                         selectedConverter = availableConverters[2];
+                        EnterData(selectedConverter);
                         break;
                     case "4":
                         selectedConverter = availableConverters[3];
+                        EnterData(selectedConverter);
                         break;
                     default:
                         Console.WriteLine("*** Invalid selection ***");
