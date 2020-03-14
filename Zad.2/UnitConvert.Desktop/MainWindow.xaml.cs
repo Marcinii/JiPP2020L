ac00e80ad179a7484df82fd32e416dd5697514cb
Ivan Grom <boris.bb.borisov@gmail.com>
2020-03-14 13:12:33 +0100

    Gotowe Zadanie 2

---
+++ Zad.2/UnitConvert.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UnitConverter;
+
+namespace UnitConvert.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> napisy_tjeden = new List<string>()
+            {
+                "K",
+                "F",
+                "C"
+            };
+            combobox_tjeden.ItemsSource = napisy_tjeden;
+            List<string> napisy_tdwa = new List<string>()
+            {
+                "K",
+                "F",
+                "C"
+            };
+            combobox_tdwa.ItemsSource = napisy_tdwa;
+            List<string> napisy_ljeden = new List<string>()
+            {
+                "M",
+                "KM",
+                "MI"
+            };
+            combobox_ljeden.ItemsSource = napisy_ljeden;
+            List<string> napisy_ldwa = new List<string>()
+            {
+                "M",
+                "KM",
+                "MI"
+            };
+            combobox_ldwa.ItemsSource = napisy_ldwa;
+            List<string> napisy_wjeden = new List<string>()
+            {
+                "KG",
+                "G",
+                "F"
+            };
+            combobox_wjeden.ItemsSource = napisy_wjeden;
+            List<string> napisy_wdwa = new List<string>()
+            {
+                "KG",
+                "G",
+                "F"
+            };
+            combobox_wdwa.ItemsSource = napisy_wdwa;
+        }
+
+        private void Button_Clickt(object sender, RoutedEventArgs e)
+        {
+            string inputValue = inputTextBoxt.Text;
+
+            decimal value = decimal.Parse(inputValue); // TryParse!
+
+            decimal result = new TemperatureConverter().Convert(combobox_tjeden.SelectedItem.ToString(), combobox_tdwa.SelectedItem.ToString(), value);
+
+            outputTextBlockt.Text = result.ToString();
+        }
+        private void Button_Clickl(object sender, RoutedEventArgs e)
+        {
+            string inputValue = inputTextBoxl.Text;
+
+            decimal value = decimal.Parse(inputValue); // TryParse!
+
+            decimal result = new UnitConverter.LengthConverter().Convert(combobox_ljeden.SelectedItem.ToString(), combobox_ldwa.SelectedItem.ToString(), value);
+
+            outputTextBlockl.Text = result.ToString();
+        }
+        private void Button_Clickw(object sender, RoutedEventArgs e)
+        {
+            string inputValue = inputTextBoxw.Text;
+
+            decimal value = decimal.Parse(inputValue); // TryParse!
+
+            decimal result = new WeightConverter().Convert(combobox_wjeden.SelectedItem.ToString(), combobox_wdwa.SelectedItem.ToString(), value);
+
+            outputTextBlockw.Text = result.ToString();
+        }
+    }
+}
---
+++ Zad.2/UnitConverter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IConverter> converters = new List<IConverter>()
+            {
+                new LengthConverter(),
+                new TemperatureConverter(),
+                new WeightConverter()
+            };
+
+            bool shouldContinue = true;
+
+            while (shouldContinue)
+            {
+                Console.WriteLine("Wybierz opcję: ");
+
+                for(int i = 0; i < converters.Count; i++)
+                {
+                    Console.WriteLine("({0}) {1}", i + 1, converters[i].Name);
+                }
+
+                string inputChoice = Console.ReadLine();
+
+
+                Console.WriteLine("Podaj jednostek z: ");
+                string unitFrom = Console.ReadLine();
+
+                Console.WriteLine("Podaj jednostkę do: ");
+                string unitTo = Console.ReadLine();
+
+                Console.WriteLine("Podaj liczbę do konwersji: ");
+                string inputValue = Console.ReadLine();
+
+                int choice = int.Parse(inputChoice); // TryParse!
+                decimal value = decimal.Parse(inputValue); // TryParse!
+
+                Console.WriteLine("Wynik konwersji: {0}", converters[choice - 1].Convert(unitFrom, unitTo, value));
+
+                shouldContinue = false;
+            }
+        }
+    }
+}
