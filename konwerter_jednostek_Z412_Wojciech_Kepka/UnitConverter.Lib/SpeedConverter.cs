7006ed40aa363048d1617c8837d38ebeea231e44
Wojciech Kępka <wojtek.kepka@protonmal.com>
2020-03-16 17:50:03 +0100

    Dodanie SpeedConverter

---
+++ konwerter_jednostek_Z412_Wojciech_Kepka/UnitConverter.Lib/SpeedConverter.cs
@@ -0,0 +1,64 @@
+﻿using static UnitConverter.Lib.Units;
+using static UnitConverter.Lib.Formulas;
+
+namespace UnitConverter.Lib
+{
+    public class SpeedConverter : IConverter
+    {
+        public double convert(double value, string fromUnit, string toUnit)
+        {
+            Unit inputUnit = UnitFromString(fromUnit);
+            Unit outputUnit = UnitFromString(toUnit);
+
+            switch (inputUnit)
+            {
+                case Unit.KilometersPerHour:
+                    if (outputUnit == Unit.MilesPerHour)
+                    {
+                        return KphToMph(value);
+                    }
+                    else if (outputUnit == Unit.Knots)
+                    {
+                        return KphToKnots(value);
+                    }
+                    else if (outputUnit == Unit.MetersPerSecond)
+                    {
+                        return KphToMps(value);
+                    }
+                    break;
+                case Unit.MilesPerHour:
+                    if (outputUnit == Unit.KilometersPerHour)
+                    {
+                        return MphToKph(value);
+                    }
+                    else if (outputUnit == Unit.Knots)
+                    {
+                        return MphToKnots(value);
+                    }
+                    else if (outputUnit == Unit.MetersPerSecond)
+                    {
+                        return MphToMps(value);
+                    }
+                    break;
+                case Unit.Knots:
+                    if (outputUnit == Unit.MilesPerHour)
+                    {
+                        return KnotsToMph(value);
+                    }
+                    else if (outputUnit == Unit.KilometersPerHour)
+                    {
+                        return KnotsToKph(value);
+                    }
+                    else if (outputUnit == Unit.MetersPerSecond)
+                    {
+                        return KnotsToMps(value);
+                    }
+                    break;
+                default:
+                    throw new UnsupportedUnit(inputUnit);
+            }
+
+            throw new IncompatibleConversionUnits(inputUnit, outputUnit);
+        }
+    }
+}
