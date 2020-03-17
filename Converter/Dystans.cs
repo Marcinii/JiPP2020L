a019a9b9e017e335ca8c8f6b070aad830ad4ae90
Agata Błaszczyk <blaszczyk.m.agata@gmail.com>
2020-03-17 19:17:07 +0100

    ConsoleApp

---
+++ Converter/Dystans.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Dystans
+    {
+        public Dystans() { }
+        public double convert(double a, string from, string to)
+        {
+            double b = a;
+
+            if (from == "km" && to == "miles")
+            {
+                b = a * 0.621371192;
+            }
+            else if (from == "miles" && to == "km")
+            {
+                b = a * 1 / 0.621371192;
+            }
+
+            return b;
+        }
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wybierz konwerter:");
+            Console.WriteLine("Temperatura (1)");
+            Console.WriteLine("Dystans (2)");
+            Console.WriteLine("Waga (3)");
+
+            int converter_type = int.Parse(Console.ReadLine());
+
+            if (converter_type == 1)
+            {
+                Console.WriteLine("Z: C/F");
+                string from = Console.ReadLine();
+                Console.WriteLine("Na: C/F");
+                string to = Console.ReadLine();
+                Console.WriteLine("Ile?");
+                double many = double.Parse(Console.ReadLine());
+
+                Temperatura converter = new Temperatura();
+
+                double b = converter.convert(many, from, to);
+                Console.WriteLine(b);
+            }
+            else if (converter_type == 2)
+            {
+                Console.WriteLine("Z: km/miles");
+                string from = Console.ReadLine();
+                Console.WriteLine("Na: miles/km");
+                string to = Console.ReadLine();
+                Console.WriteLine("Ile?");
+                double many = double.Parse(Console.ReadLine());
+
+                Dystans converter = new Dystans();
+
+                double b = converter.convert(many, from, to);
+                Console.WriteLine(b);
+            }
+            else if (converter_type == 3)
+            {
+
+                Console.WriteLine("Z: kg/lbs");
+                string from = Console.ReadLine();
+                Console.WriteLine("Na: kg/lbs");
+                string to = Console.ReadLine();
+                Console.WriteLine("Ile?");
+                double many = double.Parse(Console.ReadLine());
+
+                Masa converter = new Masa();
+
+                double b = converter.convert(many, from, to);
+                Console.WriteLine(b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
