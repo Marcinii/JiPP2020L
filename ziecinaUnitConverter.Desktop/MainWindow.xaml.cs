df861cfb1da483e33be0d1bdaf69d863cf46c642
AzerimX <cezary.ziecina@gmail.com>
2020-03-21 18:12:27 +0100

    Najpewniej final push

--- ziecinaUnitConverter.Desktop/MainWindow.xaml.cs
+++ ziecinaUnitConverter.Desktop/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ziecina_zad1;
 
 namespace ziecinaUnitConverter.Desktop
 {
@@ -20,9 +21,50 @@ namespace ziecinaUnitConverter.Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<IConverter> converters;
+        public static int chosenConverter;
+
         public MainWindow()
         {
             InitializeComponent();
+            converters = new List<IConverter>()
+            {
+                new ConvertTemperature(),
+                new ConvertMass(),
+                new ConvertLength(),
+                new ConvertVolume()
+            };
+            for (int i = 0; i < converters.Count; i++)
+            {
+                listConverters.Items.Add(converters[i].Name);
+            }
+        }
+
+        private void textValues_KeyUp(object sender, KeyEventArgs e)
+        {
+            string input = textValues.Text;
+            try
+            {
+                float whateva = Convert.ToSingle(input);
+                //blockResault.Text = "k liczba";
+                float toDisplay = MainWindow.converters[chosenConverter].Convert(listStartUnit.SelectedItem.ToString(), listEndUnit.SelectedItem.ToString(), input);
+                string toDisplayStr = toDisplay.ToString();
+                blockResault.Text = toDisplayStr;
+            }
+            catch
+            {
+                blockResault.Text = "Niepoprawne dane";
+            }
+        }
+
+        private void listConverters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MainWindow.chosenConverter = listConverters.SelectedIndex;
+            listStartUnit.ItemsSource = MainWindow.converters[chosenConverter].Units;
+            listEndUnit.ItemsSource = MainWindow.converters[chosenConverter].Units;
+
+
+            
         }
     }
 }
