e4560da6a600bf8af1acc444d27da04edb76ac21
Maciej Słowikowski <slowikowski.maciejj@gmail.com>
2020-03-22 23:59:50 +0100

    Porządek

---
+++ Modul_2/Modul_2.logic/ConverterService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_2
+
+{
+    public class ConverterService
+    {
+        public List<Ikonwerter> GetConverter()
+        {
+            return new List<Ikonwerter>
+            {
+                new Dlugosc(),
+                new Temperatura(),
+                new Czas(),
+                new Waga()
+            };
+        }
+    }
+}
---
+++ Modul_2/Modul_2.logic/Czas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_2
+{
+    public class Czas : Ikonwerter
+    {
+        public string nazwa => "Czas";
+
+        public List<string> Unit => new List<string>() {
+            "Sekunda-Minuta",
+            "Minuta-Sekunda"
+        };
+
+        public double Zamiana(double wynik, int x)
+        {
+            if (x == 1)
+                return wynik / 60;
+            if (x == 2)
+                return wynik * 60;
+            else return 0;
+        }
+    }
+}
