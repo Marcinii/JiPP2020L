a542dcb6493f27505306b578371d8652d27c6228
localadm <localadm@DESKTOP-0JJAHK2>
2020-03-22 17:31:50 +0100

    Konw-okienko

---
+++ Konwerter_Okienkowy-Z401-Piotr-Szubiczuk/Konwerter.Logika/CnF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konwerter
+{
+    public class CnF: Ikonwenter
+    {
+        public string Nazwa => "Temp_CnaF";
+
+        public List<string> Jednostki => new List<string>()
+        { 
+            "F",
+            "C"
+        };
+
+        public double Konwer(string zczego, string naco, double wartosc)
+        {
+            double fahrenhait, celcjusz;
+            fahrenhait = double.Parse(Console.ReadLine());
+            celcjusz = 5D / 9 * (fahrenhait - 32);
+            return celcjusz;
+        }
+    }
+}
