1d078c858269fde1d8008e2f129ec91e7b327e8e
BartlomiejSocik <57301283+BartlomiejSocik@users.noreply.github.com>
2020-03-22 22:32:54 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZAD2.WPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            RodzajKonwertera.ItemsSource = new List<IProgram>()
+            {
+
+                new Masa(),
+                new Odleglosc(),
+                new Temperatura(),
+                new Objetosc()
+            };
+        }
+        private void RodzajKonwertera_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            JednostkaZ.ItemsSource = ((IProgram)RodzajKonwertera.SelectedItem).j_;
+            JednostkaDo.ItemsSource = ((IProgram)RodzajKonwertera.SelectedItem).j_;
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            string DaneText = DaneWpisz.Text;
+            double DaneLiczba = double.Parse(DaneText);
+
+            double wynikLiczba = ((IProgram)RodzajKonwertera.SelectedItem).Konwersja(
+                JednostkaZ.SelectedItem.ToString(),
+                JednostkaDo.SelectedItem.ToString(),
+                DaneLiczba);
+            WynikText.Text = wynikLiczba.ToString();
+
+        }
+    }
+}
