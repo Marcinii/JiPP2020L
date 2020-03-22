a39353cbdd0cdb2d38c4a0a7ee69e9d67a7482f2
Arek Tchorzewski <TCHORZEWSKI.ARKADIUSZ@GMAIL.COM>
2020-03-22 22:57:19 +0100

    Arkadiusz Tchorzewski  - porzadki na branch 2.0

---
+++ KonwerterjJednostek2/KonwerterJednostek.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KonwerterJednostek2.Logic;
+
+namespace KonwerterJednostek.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            combobox_Rodzaj_konwersji.ItemsSource = new List<string>()
+            {
+                "Temperatura",
+                "Odleglość",
+                "Masa",
+                "Czas"     
+            };
+
+
+
+        }
+
+        private void combobox_Rodzaj_konwersji_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Temperatura")
+            {
+                combobox_Jednostka_bazowa.ItemsSource = new List<string>()
+                {
+                    "C",
+                    "F",
+                    "K"
+                };
+
+                combobox_Jednostka_wynikowa.ItemsSource = new List<string>()
+                {
+                    "C",
+                    "F",
+                    "K"
+                };
+
+            }
+
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Odleglość")
+            {
+                combobox_Jednostka_bazowa.ItemsSource = new List<string>()
+                {
+                    "M",
+                    "KM",
+                    "MI"
+                };
+
+                combobox_Jednostka_wynikowa.ItemsSource = new List<string>()
+                {
+                    "M",
+                    "KM",
+                    "MI"
+                };
+
+            }
+
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Masa")
+            {
+                combobox_Jednostka_bazowa.ItemsSource = new List<string>()
+                {
+                    "G",
+                    "KG",
+                    "F"
+                };
+
+                combobox_Jednostka_wynikowa.ItemsSource = new List<string>()
+                {
+                    "G",
+                    "KG",
+                    "F"
+                };
+
+            }
+
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Czas")
+            {
+                combobox_Jednostka_bazowa.ItemsSource = new List<string>()
+                {
+                    "G",
+                    "M",
+                    "S"
+                };
+
+                combobox_Jednostka_wynikowa.ItemsSource = new List<string>()
+                {
+                    "G",
+                    "M",
+                    "S"
+                };
+
+            }
+
+
+        }
+
+        private void button_action_Click(object sender, RoutedEventArgs e)
+        {
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Temperatura")
+            {
+
+                Temperatura_class t1 = new Temperatura_class();
+            };
+
+            }
+
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Odleglość")
+            {
+
+
+            }
+
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Masa")
+            {
+
+
+            }
+
+            if (combobox_Rodzaj_konwersji.SelectedItem == "Czas")
+            {
+
+
+            }
+        }
+    }
+}
---
+++ KonwerterjJednostek2/KonwerterjJednostek2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonwerterjJednostek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IKonwerter> Konwerters = new List<IKonwerter>()
+            {
+                new Temperatura_class(),
+                new Odleglosc_class(),
+                new Masa_class(),
+                new Czas_class()
+            };
+
+            Temperatura_class t1 = new Temperatura_class();
+            bool czy_kontynowac = true;
+
+            while (czy_kontynowac)
+            {
+                Console.WriteLine("Wybierz rodzaj konwersji: ");
+
+                for(int i =0; i<Konwerters.Count; i++)
+                {
+                    Console.WriteLine("({0}) {1}", i + 1, Konwerters[i].Name);
+                }
+
+                //pobiera rodzaj miary
+                string rodzaj_konwersji1 = Console.ReadLine();
+                int rodzaj_konwersji2 = int.Parse(rodzaj_konwersji1);
+
+                //pobiera jednostke bazową
+                Console.WriteLine("Podaj jednostke bazowa:");
+                string jednostka_bazowa1 = Console.ReadLine();
+
+                //pobiera jednostke docelową
+                Console.WriteLine("Podaj jednostke docelowa:");
+                string jednostka_docelowa = Console.ReadLine();
+
+                //pobiera wartość do konwersji
+                Console.WriteLine("Podaj liczbe do konwersji");
+                string wartosc1 = Console.ReadLine();
+
+                //konwertuje pobraną wartość na liczbę
+                double wartosc2 = double.Parse(wartosc1);
+
+                double wynik = Konwerters[rodzaj_konwersji2 - 1].Konwerter(jednostka_bazowa1, jednostka_docelowa, wartosc2);
+
+                Console.WriteLine("Wynik konwersji: {0}", wynik);
+                Console.WriteLine("");
+
+            }
+
+        }
+    }
+}
