77c3c532e96d1f49c9b54108ac199c5b5ed4c676
Krzysztof Rosłaniec <krzysztof.roslaniec@gmail.com>
2020-03-27 00:21:17 +0100

    Moduł 2 Krzysztof Rosłaniec

---
+++ Modul2/UnitConverter.Logic/IConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul2
+{
+    public interface IConverter
+    {
+        string Name { get; }
+        List<string> Units { get; }
+        double Convert(string unitFrom, string unitTo, double valueToConvert);
+    }
+}
---
+++ Modul2/UnitConverter.Logic/WeightConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul2
+{
+    public class WeightConverter : IConverter
+    {
+        public string Name => "Weight";
+
+        public List<string> Units => new List<string>()
+        {
+            "KG",
+            "LB"
+        };
+        public double Convert(string unitFrom, string unitTo, double valueToConvert)
+        {
+            if (unitFrom == "KG")
+            {
+                return valueToConvert * 2.20462262;
+            }
+            else
+            {
+                return valueToConvert * 0.45359237;
+            }
+        }
+    }
+}
