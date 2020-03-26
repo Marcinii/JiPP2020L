9a2141db818440cd85f9e9067b3208e5c8b12b09
Michał Kłosiewicz <michal.klosiewicz@student.wat.edu.pl>
2020-03-26 11:21:01 +0100

    Michal Klosiewicz - zadanie 3

---
+++ Zad3/UnitConverter.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using UnitConverter.Logic;
+
+namespace UnitConverter.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool ruch = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            converterCombobox.ItemsSource = new ConverterService().GetConverters();
+        }
+
+        private void converterCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            unitFromCombobox.ItemsSource = ((IConverter)converterCombobox.SelectedItem).Units;
+            unitToCombobox.ItemsSource = ((IConverter)converterCombobox.SelectedItem).Units;
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputText = inputTextBox.Text;
+            string inputValue = System.Convert.ToString(inputText);
+
+            string result = ((IConverter)converterCombobox.SelectedItem).Convert(
+                unitFromCombobox.SelectedItem.ToString(),
+                unitToCombobox.SelectedItem.ToString(),
+                inputValue);
+            outputTextBlock.Text = result.ToString();
+
+            if (Convert.ToString(unitFromCombobox.SelectedItem) == "24" && Convert.ToString(unitToCombobox.SelectedItem) == "12")
+            {
+                double x, y;
+                
+                int index = result.IndexOf(":");
+                if (index == 1)
+                {
+                     x = Convert.ToDouble(result.Substring(0, 1));
+                     y = Convert.ToDouble(result.Substring(2, 2));
+                }
+                else
+                {
+                     x = Convert.ToDouble(result.Substring(0, 2));
+                     y = Convert.ToDouble(result.Substring(3, 2));
+                }
+                
+                x = x * 30 + 90;
+                y = y * 6 + 90;
+                
+                Pokaz_Zegar(x,y);
+            }
+        }
+        private void Pokaz_Zegar( double min, double hour)
+        {
+            WskHour.Angle = hour;
+            WskMin.Angle = min;
+        }
+    }
+}
---
+++ Zad3/UnitConverter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IConverter> converters = new List<IConverter>()
+            {
+                new TemperatureConverter(),
+                new LengthConverter(),
+                new WeightConverter()
+            };
+
+            bool shouldContinue = true;
+
+            while (shouldContinue)
+            {
+                Console.WriteLine("Wybierz opcję: ");
+                
+                for(int i = 0; i < converters.Count; i++)
+                {
+                    Console.WriteLine("({0}) {1}", i + 1, converters[i].Name);
+                }
+
+                string inputChoice = Console.ReadLine();
+                int choice = int.Parse(inputChoice); // TryParse!
+
+                Console.WriteLine("Podaj jednostkę Z: ");
+                string unitFrom = Console.ReadLine();
+
+                Console.WriteLine("Podaj jednostkę Do: ");
+                string unitTo = Console.ReadLine();
+
+                Console.WriteLine("Podaj liczbę do konwersji: ");
+                string inputValue = Console.ReadLine();              
+                decimal value = decimal.Parse(inputValue); // TryParse!
+
+                decimal result = converters[choice - 1].Convert(unitFrom, unitTo, value);
+
+                Console.WriteLine("Wynik konwersji: {0}", result);
+
+                shouldContinue = false;
+            }
+        }
+    }
+}
