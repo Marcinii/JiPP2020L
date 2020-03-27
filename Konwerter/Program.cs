b08330c2facf413c6126c0e801b40b43dae1ed3f
BartoszJedrzynski <jendrelele@gmail.com>
2020-03-27 01:17:16 +0100

    Zadanie 2 Bartosz Jedrzynski

--- Konwerter/Program.cs
+++ Konwerter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Konwerter
 {
@@ -6,108 +7,49 @@ namespace Konwerter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wpisz: \n 1 - konwerter temperatur \n 2 - konwerter dlugosci \n 3 - konwerter mas");
 
-            int a;
+            List<IEConverter> Converters = new List<IEConverter>()
+            {
+                new WeightConverter(),
+                new LengthConverter(),
+                new TempConverter(),
+                new VolumeConverter()
 
-            a = int.Parse(Console.ReadLine());
+            };
 
-            if (a == 1)
+            Console.WriteLine("Wpisz odpowiednią nazwe konwertera");
+
+            foreach (IEConverter converter in Converters)
             {
-                ConvertTemp();
-            }
-            else if (a == 2)
-            {
-                ConvertLengths();
-            }
-            else if (a == 3)
-            {
-                ConvertMass();
+                Console.WriteLine(converter.Name);
             }
 
-            void ConvertTemp() 
+            string name = Console.ReadLine();
+            Console.WriteLine("Wartość do konwersji");
+            float value = float.Parse(Console.ReadLine());
+            Console.WriteLine("Z jakiej jednostki konwertujemy?");
+            string from = Console.ReadLine();
+            Console.WriteLine("Do jakiej jednostki konwertujemy?");
+            string to = Console.ReadLine();
+
+            switch (name) 
             {
-                Console.WriteLine("Wpisz: \n 1 - Konwertuj Celsjusze na Farenheity \n 2 - Konwertuj Farenheity na Celsjusze");
-
-                int b;
-                float temp;
-                float oldTemp;
-                
-                b = int.Parse(Console.ReadLine());
-
-                if (b == 1)
-                {
-                    Console.WriteLine("Wpisz temperature");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = (temp * 1.8f) + 32f;
-                    Console.WriteLine(oldTemp + " Celsjuszy to " + temp + " Farenheitow");
-                }
-                else if (b == 2)
-                {
-                    Console.WriteLine("Wpisz temperature");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = (temp - 32f) / 1.8f;
-                    Console.WriteLine(oldTemp + " Farenheitow to " + temp + " Celsjuszy");
-                }
-            }
-
-            void ConvertLengths() 
-            {
-                Console.WriteLine("Wpisz: \n 1 - Konwertuj kilometry na mile \n 2 - Konwertuj mile na kilometry");
-
-                int b;
-                float temp;
-                float oldTemp;
-
-                b = int.Parse(Console.ReadLine());
-
-                if (b == 1)
-                {
-                    Console.WriteLine("Wpisz dlugosc");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp * 0.62137f;
-                    Console.WriteLine(oldTemp + " kilometrow to " + temp + " mil");
-                }
-                else if (b == 2)
-                {
-                    Console.WriteLine("Wpisz dlugosc");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp / 0.62137f;
-                    Console.WriteLine(oldTemp + " mil to " + temp + " kilometrow");
-                }
-            }
-
-            void ConvertMass()
-            {
-                Console.WriteLine("Wpisz: \n 1 - Konwertuj kilogramy na funty \n 2 - Konwertuj funty na kilogramy");
-
-                int b;
-                float temp;
-                float oldTemp;
-
-                b = int.Parse(Console.ReadLine());
-
-                if (b == 1)
-                {
-                    Console.WriteLine("Wpisz mase");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp * 2.2046f;
-                    Console.WriteLine(oldTemp + " kilogramow to " + temp + " funtow");
-                }
-                else if (b == 2)
-                {
-                    Console.WriteLine("Wpisz dlugosc");
-                    temp = float.Parse(Console.ReadLine());
-                    oldTemp = temp;
-                    temp = temp / 2.2046f;
-                    Console.WriteLine(oldTemp + " funtow to " + temp + " kilogramy");
-                }
+                case "Waga":
+                    Converters[0].ConvertUnit(from,to,value);
+                    break;
+                case "Temperatury":
+                    Converters[2].ConvertUnit(from, to, value);
+                    break;
+                case "Dlugosc":
+                    Converters[1].ConvertUnit(from, to, value);
+                    break;
+                case "Pojemnosc":
+                    Converters[3].ConvertUnit(from, to, value);
+                    break;
             }
         }
-    }
+
+        
+}
+       
 }
