482669db8d9c52bd0be1ddb2a4431be7f908ef93
BartlomiejSocik <57301283+BartlomiejSocik@users.noreply.github.com>
2020-03-28 14:51:41 +0100

    Add files via upload

---
+++ Czas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD3
+{
+    public class Czas
+    {
+        string Name => "Czas";
+
+        
+        public double Z24hna12h(int h, int min, bool CzyAM, bool Czy24h)
+        {
+            double hmin = 0;
+            if (Czy24h == true)
+            {
+                if (h > 12)
+                {
+                    h = h - 11;
+                    CzyAM = false;
+                    hmin = (double)h + ((double)min / 100);
+                    return hmin;
+
+                }
+                else if (h <= 12)
+                {
+                    h = h + 1;
+                    CzyAM = true;
+                    hmin = (double)h + ((double)min / 100);
+                    return hmin;
+                }
+                else { return 0; }
+            }
+            else if (Czy24h == false)
+            {
+                if (CzyAM == false)
+                {
+                    h = h + 13;
+                    hmin = (double)h + ((double)min / 100);
+                    return hmin;
+                }
+                else if (CzyAM == true)
+                {
+                    h = h + 1;
+                    hmin = (double)h + ((double)min / 100);
+                    return hmin;
+                }
+                else
+                { return 0; }
+            }
+            else
+            { return 0; }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZAD3;
 
-namespace ZAD2.WPF
+namespace ZAD3.WPF
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
@@ -33,6 +34,18 @@ namespace ZAD2.WPF
                 new Temperatura(),
                 new Objetosc()
             };
+            Czy12h.ItemsSource = new List<string>()
+            {
+                "12h",
+                "24h"
+            };
+            Minuty.ItemsSource = new List<int>()
+            {
+                0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+                31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59
+            };
+            
+
         }
         private void RodzajKonwertera_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -50,6 +63,103 @@ namespace ZAD2.WPF
                 DaneLiczba);
             WynikText.Text = wynikLiczba.ToString();
 
+        }
+
+        public void Czy12h_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            bool CzyAM = true;
+            bool Czy24h = true;
+            int h = 0;
+            int min = 0;
+            h= Godziny.SelectedIndex;
+            min =Minuty.SelectedIndex;
+            if (Czy12h.SelectedItem=="12h")
+            {
+                Godziny.ItemsSource = new List<int>()
+                {
+                1,2,3,4,5,6,7,8,9,10,11,12
+                };
+                Czy24h = false;
+            }
+            else if(Czy12h.SelectedItem == "24h")
+            {
+                Godziny.ItemsSource = new List<int>()
+                {
+                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+                };
+                Czy24h = true;
+            }
+
+            if(Czy12h.SelectedItem == "12h")
+            {
+                AMPM.ItemsSource = new List<string>()
+                {
+                    "AM",
+                    "PM"
+                };
+                Czy24h = false;
+            }
+            else
+            {
+                AMPM.ItemsSource = new List<string>()
+                {};
+            }
+
+            if (AMPM.SelectedItem == "PM")
+            {
+                CzyAM = false;
+            }
+            else if (AMPM.SelectedItem == "AM")
+            {
+                CzyAM = true;
+            }
+            Czas Wynik = new Czas();
+
+            double pomocniczy = Wynik.Z24hna12h(h,min,CzyAM,Czy24h);
+            int mm = (int)((pomocniczy * 100) % 100);
+            int hh = (int)((pomocniczy) % 100);
+            if (h > 12 && Czy24h==true)
+            {
+                if (mm < 10)
+                {
+
+                    Wyswietlacz.Text = hh.ToString() + ":" + "0" + mm.ToString() + " PM";
+                }
+                else
+                {
+                    Wyswietlacz.Text = hh.ToString() + ":" + mm.ToString() + " PM";
+                }
+            }
+            else if(h <= 12 && Czy24h == true)
+            {
+                if (mm < 10)
+                {
+
+                    Wyswietlacz.Text = hh.ToString() + ":" + "0" + mm.ToString() + " AM";
+                }
+                else
+                {
+                    Wyswietlacz.Text = hh.ToString() + ":" + mm.ToString() + " AM";
+                }
+            }
+            else
+            {
+                if (mm < 10)
+                {
+
+                    Wyswietlacz.Text = hh.ToString() + ":" + "0" + mm.ToString();
+                }
+                else
+                {
+                    Wyswietlacz.Text = hh.ToString() + ":" + mm.ToString();
+                }
+            }
+           
+            GodzinowaObrot.Angle = 0;
+            MinutowaObrot.Angle = 0;
+
+            GodzinowaObrot.Angle = ((hh + 3) * 30);
+            MinutowaObrot.Angle = (mm + 15) * 6;
         }
     }
 }
