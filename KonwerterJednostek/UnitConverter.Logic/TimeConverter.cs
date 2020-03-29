25e1ecc083a7ed2efd4aa7605700fa07210504f1
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-03-29 19:44:27 +0200

    added methods to verify correctness of time

--- KonwerterJednostek/UnitConverter.Logic/TimeConverter.cs
+++ KonwerterJednostek/UnitConverter.Logic/TimeConverter.cs
@@ -19,13 +19,41 @@ namespace UnitConverter.Logic
 
         public double Convert(int from, int to, double valueToConvert)
         {
+            double correctMinuteValues = checkCorrectnessOfMinuteValue(valueToConvert);
+            decimal correctTime = (decimal)checkCorrectnessOfHourValue(correctMinuteValues);
             //list all of available units and their conversion values
-            double[][] factor =
+            decimal[][] factor =
                 {
-                    new double[] {valueToConvert, valueToConvert+12}, //12
-                    new double[] {valueToConvert-12, valueToConvert}, //24
+                    new decimal[] {correctTime, correctTime + 12}, //12
+                    new decimal[] {correctTime<12?correctTime:correctTime-12, correctTime}, //24
                 };
-            return factor[from][to];
+            decimal result = factor[from][to];
+            decimal correctTimeAfterCheck = (decimal)checkCorrectnessOfHourValue((double)result);
+            return (double)correctTimeAfterCheck;
         }
+
+        private double checkCorrectnessOfMinuteValue(double valueToCheck) {
+            int hourVariable = (int)valueToCheck;
+            decimal minuteVariable = (decimal)valueToCheck - hourVariable;
+            decimal correctTimeFormat = minuteVariable >= 0.6M ?
+                hourVariable + 1 + (decimal.Subtract(minuteVariable,0.6M)) : (decimal)valueToCheck;
+            return (double)correctTimeFormat;
+        }
+
+        private double checkCorrectnessOfHourValue(double valueToCheck)
+        {
+            int hourVariable = (int)valueToCheck;            
+            if (hourVariable >= 24)
+            {
+                decimal minuteVariable = (decimal)valueToCheck - hourVariable;
+                int modulo = hourVariable % 24;
+                return modulo + (double)minuteVariable;
+            }
+            else {
+                return valueToCheck;
+            }           
+        }
+
+
     }
 }
