5dc595080b7bfeec5dd74b54bbfc313c098e6926
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-04-13 22:00:08 +0200

    Added 'Entity Framework' to project

---
+++ KonwerterJednostek/UnitConverter.Desktop/ConverterDatabaseModel.cs
@@ -0,0 +1,35 @@
+namespace UnitConverter.Desktop
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ConverterDatabaseEntities : DbContext
+    {
+        // Your context has been configured to use a 'ConverterDatabaseModel' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'UnitConverter.Desktop.ConverterDatabaseModel' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'ConverterDatabaseModel' 
+        // connection string in the application configuration file.
+        public ConverterDatabaseEntities()
+            : base("name=ConverterDatabaseModel")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+
+
+
+        public DbSet<ConverterData> ConverterDatas { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
