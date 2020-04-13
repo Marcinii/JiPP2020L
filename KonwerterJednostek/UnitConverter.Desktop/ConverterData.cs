2f49da452a7eba5f5486cb889798e37892c69827
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-04-13 22:02:53 +0200

    added ConverterData entity

---
+++ KonwerterJednostek/UnitConverter.Desktop/ConverterData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.Desktop
+{
+    public class ConverterData
+    {
+        public int Id { get; set; }
+        public string UsedConverter { get; set; }
+        public string UnitFrom { get; set; }
+        public string UnitTo { get; set; }
+        public string InputValue { get; set; }
+        public string OutputValue { get; set; }
+        public DateTime? ConvertDate { get; set; }
+    }
+}
