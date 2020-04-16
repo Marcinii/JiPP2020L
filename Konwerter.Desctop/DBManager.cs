c289b43953cbe990f85e1a29151b0db933c99710
aniaw <aniaw@LAPTOP-4LTOC4VK>
2020-04-16 19:54:02 +0200

    zad 4

---
+++ Konwerter.Desctop/DBManager.cs
@@ -0,0 +1,122 @@
+﻿using KonwerterJednostek;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Konwerter.Desctop
+{
+    class DBManager
+    {
+        private List<Statistic> statistics;
+
+        private int skipIndexCount;
+
+        private const int RowsPerPage = 20;
+
+        public DBManager()
+        {
+            LoadDB();
+        }
+
+        public List<Statistic> LoadDB()
+        {
+            using (StatisticDataEntities1 context = new StatisticDataEntities1())
+            {
+                statistics = context.Statistics.OrderBy(x => x.Id).ToList();
+                return statistics;
+            }
+        }
+
+        public void SaveToDB(Statistic statistic, DataGrid dataGrid)
+        {
+            using (StatisticDataEntities1 context = new StatisticDataEntities1())
+            {
+                context.Statistics.Add(statistic);
+                context.SaveChanges();
+
+                statistics = context.Statistics.ToList();
+                dataGrid.ItemsSource = statistics;
+            }
+        }
+
+        private List<Statistic> ByDate(DateTime? dateFrom, DateTime? dateTo)
+        {
+            if (dateFrom != null)
+            {
+                statistics = statistics.Where(statistic => statistic.conversion_data >= dateFrom).ToList();
+            }
+            else if (dateTo != null)
+            {
+                statistics = statistics.Where(statistic => statistic.conversion_data <= dateTo).ToList();
+            }
+            else if (dateTo != null && dateFrom != null)
+            {
+                statistics = statistics.Where(statistic => statistic.conversion_data >= dateFrom && statistic.conversion_data <= dateTo).ToList();
+            }
+
+            return statistics;
+        }
+
+        private List<Statistic> ByConverter(string name)
+        {
+            if (name != "Wszystkie")
+            {
+                statistics = statistics.Where(statistic => statistic.converter_type == name).ToList();
+            }
+            return statistics;
+        }
+
+        public List<Statistic> Filter(string name, DateTime? dateFrom, DateTime? dateTo)
+        {
+            LoadDB();
+            ByConverter(name);
+            ByDate(dateFrom, dateTo);
+            skipIndexCount = 0;
+            return statistics.Take(RowsPerPage).ToList();
+        }
+
+        public List<Statistic> TakeNext()
+        {
+            if(skipIndexCount < statistics.Count - RowsPerPage)
+            {
+                skipIndexCount += RowsPerPage;
+            }
+            
+            return statistics.Skip(skipIndexCount).Take(RowsPerPage).ToList();
+        }
+
+        public List<Statistic> TakePrev()
+        {
+            if(skipIndexCount > 0)
+            {
+                skipIndexCount -= RowsPerPage;
+            }
+            return statistics.Skip(skipIndexCount).Take(RowsPerPage).ToList();
+        }
+
+        public List<string> Top3c()
+        {
+            Dictionary<string, int> konwertery = new Dictionary<string, int>();
+            foreach(Statistic s in statistics)
+            {
+                if(!konwertery.Keys.ToList().Exists(x => x == s.converter_type))
+                {
+                    konwertery.Add(s.converter_type, 0);
+                }
+            }
+
+            List<string> klucze = konwertery.Keys.ToList();
+
+            foreach (string key in klucze)
+            {
+                konwertery[key] = statistics.Count(x => x.converter_type == key);
+            }
+
+            konwertery = konwertery.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
+            return konwertery.Keys.Take(3).ToList();
+        }
+    }
+}
--- Konwerter.Desctop/MainWindow.xaml.cs
+++ Konwerter.Desctop/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace Konwerter.Desctop
         private List<string> hours_1 = new List<string>();
         private List<string> minutes = new List<string>();
 
+        private DBManager db = new DBManager();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +38,16 @@ namespace Konwerter.Desctop
                 hours_1.Add(i.ToString());
             }
             Godzina.ItemsSource = hours_1;
+
+            List<string> konwertery = new List<string>();
+            konwertery.Add("Wszystkie");
+            konwertery.Add(nameof(KonwerterOdleglosc));
+            konwertery.Add(nameof(KonwerterPredkosc));
+            konwertery.Add(nameof(KonwerterTemperatura));
+            konwertery.Add(nameof(KonwerterWaga));
+            konwertery.Add("KonwerterCzas");
+            selectConverter.ItemsSource = konwertery;
+            selectConverter.SelectedIndex = 0;
         }
 
         private void TempCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -47,6 +59,16 @@ namespace Konwerter.Desctop
                 string temperaturaZ = (string)((ComboBoxItem)((ComboBox)this.TempComboboxZ).SelectedValue).Content;
                 string temperaturaNa = (string)((ComboBoxItem)((ComboBox)this.TempComboboxNa).SelectedValue).Content;
                 this.TempWynik.Text = Math.Round(kt.Konwertuj(temperaturaZ, temperaturaNa, wartosc), 1, MidpointRounding.ToEven).ToString();
+
+                db.SaveToDB(new Statistic()
+                {
+                    conversion_data = DateTime.Now,
+                    converter_type = nameof(KonwerterTemperatura),
+                    unit_from = temperaturaZ,
+                    unit_to = temperaturaNa,
+                    insert_data = this.TemperaturaTextBox.Text, 
+                    output_data = this.TempWynik.Text
+                }, dataGrid);
             }
             return;
         }
@@ -60,6 +82,16 @@ namespace Konwerter.Desctop
                 string odlegloscZ = (string)((ComboBoxItem)((ComboBox)this.OdlComboboxZ).SelectedValue).Content;
                 string odlegloscNa = (string)((ComboBoxItem)((ComboBox)this.OdlComboboxNa).SelectedValue).Content;
                 this.OdlWynik.Text = Math.Round(ko.Konwertuj(odlegloscZ, odlegloscNa, wartosc), 3, MidpointRounding.ToEven).ToString();
+
+                db.SaveToDB(new Statistic()
+                {
+                    conversion_data = DateTime.Now,
+                    converter_type = nameof(KonwerterOdleglosc),
+                    unit_from = odlegloscZ,
+                    unit_to = odlegloscNa,
+                    insert_data = this.OdlegloscTextBox.Text,
+                    output_data = this.OdlWynik.Text
+                }, dataGrid);
             }
             return;
         }
@@ -73,6 +105,16 @@ namespace Konwerter.Desctop
                 string PredkoscZ = (string)((ComboBoxItem)((ComboBox)this.PreComboboxZ).SelectedValue).Content;
                 string PredkoscNa = (string)((ComboBoxItem)((ComboBox)this.PreComboboxNa).SelectedValue).Content;
                 this.PreWynik.Text = Math.Round(kp.Konwertuj(PredkoscZ, PredkoscNa, wartosc), 1, MidpointRounding.ToEven).ToString();
+
+                db.SaveToDB(new Statistic()
+                {
+                    conversion_data = DateTime.Now,
+                    converter_type = nameof(KonwerterPredkosc),
+                    unit_from = PredkoscZ,
+                    unit_to = PredkoscNa,
+                    insert_data = this.PredkoscTextBox.Text,
+                    output_data = this.PreWynik.Text
+                }, dataGrid);
             }
             return;
         }
@@ -86,6 +128,16 @@ namespace Konwerter.Desctop
                 string WagaZ = (string)((ComboBoxItem)((ComboBox)this.WagaComboboxZ).SelectedValue).Content;
                 string WagaNa = (string)((ComboBoxItem)((ComboBox)this.WagaComboboxNa).SelectedValue).Content;
                 this.WagaWynik.Text = Math.Round(kw.Konwertuj(WagaZ, WagaNa, wartosc), 1, MidpointRounding.ToEven).ToString();
+
+                db.SaveToDB(new Statistic()
+                {
+                    conversion_data = DateTime.Now,
+                    converter_type = nameof(KonwerterWaga),
+                    unit_from = WagaZ,
+                    unit_to = WagaNa,
+                    insert_data = this.WagaTextBox.Text,
+                    output_data = this.WagaWynik.Text
+                }, dataGrid);
             }
             return;
         }
@@ -107,8 +159,19 @@ namespace Konwerter.Desctop
                 {
                     CzasWynik.Text = (Convert.ToInt32(Godzina.SelectedItem) - 12).ToString() + ":" + Minuty.SelectedItem + "PM";
                 }
+
+                SetClock(Convert.ToInt32(Godzina.SelectedItem), Convert.ToInt32(Minuty.SelectedItem));
+
+                db.SaveToDB(new Statistic()
+                {
+                    conversion_data = DateTime.Now,
+                    converter_type = "KonwerterCzas",
+                    unit_from = "24h",
+                    unit_to = "12h",
+                    insert_data = Godzina.SelectedItem + ":" + Minuty.SelectedItem,
+                    output_data = CzasWynik.Text
+                },dataGrid);
             }
-            SetClock(Convert.ToInt32(Godzina.SelectedItem), Convert.ToInt32(Minuty.SelectedItem));
         }
 
         private void Minuty_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -123,7 +186,19 @@ namespace Konwerter.Desctop
                 {
                     CzasWynik.Text = (Convert.ToInt32(Godzina.SelectedItem) - 12).ToString() + ":" + Minuty.SelectedItem + "PM";
                 }
+
                 SetClock(Convert.ToInt32(Godzina.SelectedItem), Convert.ToInt32(Minuty.SelectedItem));
+
+                db.SaveToDB(new Statistic()
+                {
+                    conversion_data = DateTime.Now,
+                    converter_type = "KonwerterCzas",
+                    unit_from = "24h",
+                    unit_to = "12h",
+                    insert_data = Godzina.SelectedItem + ":" + Minuty.SelectedItem,
+                    output_data = CzasWynik.Text
+                }, dataGrid);
+
             }
         }
 
@@ -143,6 +218,33 @@ namespace Konwerter.Desctop
         {
             Storyboard storyboard = (Storyboard)Resources["ClockOnStoryboard"];
             storyboard.Begin();
+        }
+
+        private void selectConverter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string konwerter = selectConverter.SelectedItem as string;
+            dataGrid.ItemsSource = db.Filter(konwerter, dateFrom.SelectedDate, dateTo.SelectedDate);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string konwerter = selectConverter.SelectedItem as string;
+            dataGrid.ItemsSource = db.Filter(konwerter, dateFrom.SelectedDate, dateTo.SelectedDate);
+        }
+
+        private void ButtonClickPrev(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.TakePrev();
+        }
+
+        private void ButtonClickNext(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = db.TakeNext();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            topKonwersje.ItemsSource = db.Top3c();
         }
     }
 }
