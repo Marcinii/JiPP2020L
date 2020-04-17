96bedb3ad9a76cb3a1a23d75cfe781c53343eb23
Agata Błaszczyk <blaszczyk.m.agata@gmail.com>
2020-04-17 22:12:14 +0200

    Connected to DB

--- Converter.Desktop/MainWindow.xaml.cs
+++ Converter.Desktop/MainWindow.xaml.cs
@@ -21,6 +21,10 @@ namespace Converter.Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        int strona = 0;
+        int logperpage = 10;
+        int maxpage = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +37,10 @@ namespace Converter.Desktop
                 new Powierzchnia(),
                 new Zegar()
             };
+            FiltrTyp.ItemsSource = Category.ItemsSource;
+
+            
+                
 
             Tarcza.Visibility = Visibility.Hidden;
             Godzinowa.Visibility = Visibility.Hidden;
@@ -85,13 +93,106 @@ namespace Converter.Desktop
                 Converted.Content = res;
                 Minutowa.RenderTransform = new RotateTransform(360/60*minutes);
                 Godzinowa.RenderTransform = new RotateTransform(360/12*hours+360.0/60/12*minutes);
-
             }
             else
             {
                 double.TryParse(Toconvert.Text, out double value);
                 Converted.Content = ((IConverter)Category.SelectedItem).convert(value, UnitFrom.Text, UnitTo.Text);
             }
+
+            using (converterEntities context = new converterEntities())
+            {
+                Log log = new Log()
+                {
+
+                    typ = ((IConverter)Category.SelectedItem).ToString(),
+                    jednostkaz = UnitFrom.Text,
+                    jednostkado = UnitTo.Text,
+                    data = System.DateTime.Now,
+                    wartoscprzed = Toconvert.Text,
+                    wartoscpo = Converted.Content.ToString()
+                };
+
+                context.Log.Add(log);
+                context.SaveChanges();
+            }
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            using (converterEntities context = new converterEntities())
+            {
+                List<Log> logi = zwrocOdfiltrowaneLogi(context).ToList();
+                LogList.ItemsSource = logi.Take(logperpage);
+                maxpage = logi.Count()/10;
+            }
+
+            strona = 0;
+
+        }
+
+        private void LogList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void pageback(object sender, RoutedEventArgs e)
+        {
+            strona--;
+
+            using (converterEntities context = new converterEntities())
+            {
+                LogList.ItemsSource = zwrocOdfiltrowaneLogi(context).ToList().Skip(strona*10).Take(logperpage);
+            }
+        }
+
+        private void pagenext(object sender, RoutedEventArgs e)
+        {
+            if (strona != maxpage)
+            {
+                strona++;
+            }
+            
+            using (converterEntities context = new converterEntities())
+            {
+                LogList.ItemsSource = zwrocOdfiltrowaneLogi(context).ToList().Skip(strona*10).Take(logperpage);
+            }
+        }
+
+        private void showtop3(object sender, RoutedEventArgs e)
+        {
+            using (converterEntities context = new converterEntities())
+            {
+                LogList.ItemsSource = zwrocOdfiltrowaneLogi(context).GroupBy(l => new { l.typ, l.jednostkaz, l.jednostkado })
+                    .Select(g => new { g.Key.typ, g.Key.jednostkaz, g.Key.jednostkado, count = g.Count() })
+                    .OrderByDescending(g => g.count)
+                    .ToList()
+                    .Take(3);
+            }
+        }
+
+        private IQueryable<Log> zwrocOdfiltrowaneLogi(converterEntities context)
+        {
+            IQueryable<Log> log = context.Log.Where(l => l.typ == l.typ);
+            if (((IConverter)FiltrTyp.SelectedItem) != null)
+            {
+                string typ = ((IConverter)FiltrTyp.SelectedItem).ToString();
+                log = log.Where(l => l.typ == typ);
+            }
+
+            if (Zakresod.ToString() != null && Zakresod.ToString() != "")
+            {
+                DateTime zakresod = DateTime.Parse(Zakresod.ToString());
+                log = log.Where(l => l.data > zakresod);
+            }
+
+            if (Zakresdo.ToString() != null && Zakresdo.ToString() != "")
+            {
+                DateTime zakresdo = DateTime.Parse(Zakresdo.ToString());
+                log = log.Where(l => l.data <= zakresdo);
+            }
+
+            return log;
         }
     }
 }
