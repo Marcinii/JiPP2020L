90cff356b60803d1c8eaaa7106960ebecf492a19
atchorzewski <61107656+atchorzewski@users.noreply.github.com>
2020-05-21 21:00:57 +0200

    Lab6 Wskaźnik długiek operacji i Taski V1

---
+++ Wskaźnik długiej operacji/KonwerterjJednostek+Zegarek+Baza/KonwerterJednostek2.Logic/Masa_class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonwerterJednostek2.Logic
+{
+    public class Masa_class : IKonwerter
+    {
+        public string Name => "Masa";
+
+        public List<string> Units => new List<string>()
+        {
+            "KG",
+            "F",
+            "G"
+        };
+
+        public double Konwerter(string jednostka_bazowa, string jednostka_docelowa, double wartosc)
+        {
+            double wynik = 0;
+
+            //przelicza funt na kilogram
+            if (jednostka_bazowa == "F" && jednostka_docelowa == "KG")
+            {
+                //Console.WriteLine("Ilosc Kilogramow: ");
+                wynik = wartosc * 0.45359237;
+            }
+
+            //przelicza kilogram na Funt
+            if (jednostka_bazowa == "KG" && jednostka_docelowa == "F")
+            {
+                //Console.WriteLine("Ilosc Funtow: ");
+                wynik = wartosc / 0.45359237;
+            }
+
+            //przelicza gram na kilogram
+            if (jednostka_bazowa == "G" && jednostka_docelowa == "KG")
+            {
+                //Console.WriteLine("Ilosc Kilogramow: ");
+                wynik = wartosc /1000;
+            }
+
+            //przelicza kilogram na gram
+            if (jednostka_bazowa == "KG" && jednostka_docelowa == "G")
+            {
+                //Console.WriteLine("Ilosc Kilogramow: ");
+                wynik = wartosc * 1000;
+            }
+
+            return wynik;
+        }
+    }
+}
