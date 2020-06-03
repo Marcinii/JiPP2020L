a00bca2a705e0e67e2d5282e8cf27027b6f7a593
SSJIV <slawomirsawicki1995@gmail.com>
2020-06-03 02:21:10 +0200

    Sławomir Sawicki - zadanie 4

---
+++ Konwerter - zadanie 4/Konwerter/Konwerter.Logic/KonwerterCzasu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwerter.Logic
+{
+    public class KonwerterCzasu : IKonwerterDomyslny
+    {
+        public override string Nazwa { get => "Czas"; }
+
+        public KonwerterCzasu()
+        {
+            SlownikWartosci = new Dictionary<string, double>()
+            {
+                {"min", 1},
+                {"h", 60},
+                {"s", 1d/60},
+            };
+        }
+
+    }
+}
