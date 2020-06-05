a40e8b78806d7b2a43d60b1dc8c1c1a5478c255f
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-06-05 16:32:36 +0200

    added MajorMarketIndiceDataModel class

---
+++ StockExchangeApp/StockExchangeApp/MajorMarketIndiceDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeApp.Desktop
+{
+    [Table("MajorMarketIndice")]
+    public class MajorMarketIndiceDataModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? MajorIndice { get; set; }
+        public DateTime? IndiceDate { get; set; }
+    }
+}
