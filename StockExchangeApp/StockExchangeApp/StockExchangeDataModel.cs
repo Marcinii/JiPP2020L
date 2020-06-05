85d4bf15a7a87c588454f573812c6d6d750f0b53
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-06-05 16:32:55 +0200

    created StockExchangeDataModel class

---
+++ StockExchangeApp/StockExchangeApp/StockExchangeDataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockExchangeApp.Desktop
+{
+    [Table("StockExchange")]
+    public class StockExchangeDataModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public int NumberOfStocksToCompute { get; set; }
+        public string TypeOfTransaction { get; set; }
+        public int AmountOfStocksToExchange { get; set; }
+        public string Stocks_1 { get; set; }
+        public string Stocks_2 { get; set; }
+        public string Stocks_3 { get; set; }
+        public string Stocks_4 { get; set; }
+        public DateTime? StockExchangeDate { get; set; }
+    }
+}
