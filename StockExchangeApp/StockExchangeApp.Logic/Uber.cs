cb7f5158ef16f21da7e1134c87e893a3f9d7b1bf
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-06-05 16:28:51 +0200

    created 'Uber' class inherited from the IStock

---
+++ StockExchangeApp/StockExchangeApp.Logic/Uber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeApp.Logic
+{
+    class Uber: IStock
+    {
+        public string Name => "Uber";
+        public string Country => "US";
+
+        public decimal Commission(int numberOfStocks, int typeOfTransaction)
+        {
+            decimal[][] arrayOfCommissions =
+                {
+                    new decimal[] {0.1M, 0.1M}, //below 10 stocks: buy, sell
+                    new decimal[] {0.2M, 0.2M} // above 10 stocks: buy, sell
+                };
+            if (numberOfStocks < 10)
+                return numberOfStocks * arrayOfCommissions[0][typeOfTransaction];
+            else
+                return numberOfStocks * arrayOfCommissions[1][typeOfTransaction];
+        }
+    }
+}
