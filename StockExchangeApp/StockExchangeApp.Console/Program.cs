9c924081aeb7602098689340f817aa23150a5ed4
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-06-05 16:26:00 +0200

    added 'console' module

---
+++ StockExchangeApp/StockExchangeApp.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using StockExchangeApp.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
