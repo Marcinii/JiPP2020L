50fe0e29c449f91121f562fda6adf6cc201558aa
kamilzz <kkarwowski@poczta.wwsi.edu.pl>
2020-06-05 16:26:30 +0200

    created IStock interface

---
+++ StockExchangeApp/StockExchangeApp.Logic/IStock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeApp.Logic
+{
+    public interface IStock
+    {
+        string Name { get; }
+        string Country { get; }
+
+        decimal Commission(int numberOfStocks, int typeOfTransaction);              
+    }
+}
