17971fa1709c0d82b40e9100cf5d1e296e9a30ae
JMichalsk <michalskjul@gmail.com>
2020-06-21 17:15:44 +0200

    Created Data Capability converter logic

---
+++ Logic/Services/DataCapacityConvertingService.cs
@@ -0,0 +1,58 @@
+﻿using Logic.Services.Interfaces;
+
+namespace Logic.Services
+{
+    public class DataCapacityConvertingService : IDataCapacityConvertingService
+    {
+        private float _bits;
+        private float _kiloBits;
+        private float _megaBits;
+        private float _bytes;
+        private float _kiloBytes;
+        private float _megaBytes;
+
+        public float Bites
+        {
+            get => _bits;
+            set
+            {
+                _bits = value;
+                _kiloBits = value / 1000;
+                _megaBits = value / 1000000;
+                _bytes = value * 0.125f;
+                _kiloBytes = value * 0.000125f;
+                _megaBytes = value * 0.000000125f;
+            }
+        }
+
+        public float KiloBits
+        {
+            get => _kiloBits;
+            set => _bits = value * 1000;
+        }
+
+        public float MegaBits
+        {
+            get => _megaBits;
+            set => Bites = value * 1000000;
+        }
+
+        public float Bytes
+        {
+            get => _bytes;
+            set => Bites = value * 8;
+        }
+
+        public float KiloBytes
+        {
+            get => _kiloBytes;
+            set => Bites = value * 8000;
+        }
+
+        public float MegaBytes
+        {
+            get => _megaBytes;
+            set => Bites = value * 8000000;
+        }
+    }
+}
---
+++ Logic/Services/Interfaces/IDataCapacityConvertingService.cs
@@ -0,0 +1,12 @@
+﻿namespace Logic.Services.Interfaces
+{
+    public interface IDataCapacityConvertingService
+    {
+        float Bites { get; set; }
+        float KiloBits { get; set; }
+        float MegaBits { get; set; }
+        float Bytes { get; set; }
+        float KiloBytes { get; set; }
+        float MegaBytes { get; set; }
+    }
+}
