9f121f8eed5e7101370e9e2e533d7d7a234f5a44
Lukakot <lukaszkot95@gmail.com>
2020-08-11 20:17:05 +0200

    Zad2Poprawka

---
+++ Zad2Poprawka/Converter/Converter.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Converter.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            converterComboBox.ItemsSource = new List<IConverter>()
+            {
+                new LenghtConverter(),
+                new TemperatureConverter(),
+                new QuantityConverter(),
+                new WeightConverter()
+            };
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            unitFromComboBox.ItemsSource = ((IConverter)converterComboBox.SelectedItem).Units;
+            unitToComboBox.ItemsSource = ((IConverter)converterComboBox.SelectedItem).Units;
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string inputText = inputTextBox.Text;
+            decimal inputValue = decimal.Parse(inputText);
+
+            decimal result = ((IConverter)converterComboBox.SelectedItem).Convert(
+                unitFromComboBox.SelectedItem.ToString(),
+                unitToComboBox.SelectedItem.ToString(),
+                inputValue);
+
+            outputTextBlock.Text = result.ToString();
+        }
+
+   
+    }
+}
---
+++ Zad2Poprawka/Converter/Converter/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IConverter> converters = new List<IConverter>
+            {
+                new LenghtConverter(),
+                new TemperatureConverter(),
+                new WeightConverter(),
+                new QuantityConverter()
+            };
+
+
+
+            bool shouldContinue = true;
+            while (shouldContinue)
+            {
+                Console.WriteLine("Wybierz konwerter: \n");
+
+                for (int i = 0; i < converters.Count; i++)
+                {
+                    Console.WriteLine("({0}) {1}", i + 1, converters[i].name);
+                }
+
+                string inputChoice = Console.ReadLine();
+
+                List<string> units = converters[int.Parse(inputChoice) - 1].Units;
+                Console.WriteLine("\nDostępne jednostki wybranego konwertera: ");
+
+                for (int i = 0; i < units.Count; i++)
+                {
+                    Console.WriteLine(units[i]);
+                }
+
+                Console.WriteLine("\n");
+
+                Console.WriteLine("Podaj jednoste z: ");
+                string unitFrom = Console.ReadLine();
+
+                Console.WriteLine("Podaj jednoste do: ");
+                string unitTo = Console.ReadLine();
+
+                Console.WriteLine("Podaj liczbe do konwersji: ");
+                string inputValue = Console.ReadLine();
+
+                int choice = int.Parse(inputChoice);
+                int intInputValue = int.Parse(inputValue);
+
+                decimal result = converters[choice - 1].Convert(unitFrom, unitTo, intInputValue);
+
+                Console.WriteLine("Wynik konwersji: {0}", result);
+            }
+        }
+
+
+    }
+}
