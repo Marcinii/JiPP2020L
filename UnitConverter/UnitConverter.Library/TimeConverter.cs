f3b17c22a6d4c375a663bf2d9125adeab916d3ac
Krzysztof Karpieszuk <krzysztof.karpieszuk@gmail.com>
2020-08-15 18:11:36 +0200

    Zad. 3 - Add time converter

---
+++ UnitConverter/UnitConverter.Library/TimeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConverter.Library
+{
+    class TimeConverter : IConverter
+    {
+        public string Name => "Time";
+        public List<string> Units => new List<string> { "am", "pm", "24h" };
+
+        public double Convert(double inputValue, string inputUnit)
+        {
+            if (inputUnit == "am")
+            {
+                return inputValue * 2;
+            }
+            else if (inputUnit == "pm")
+            {
+                return inputValue;
+            }
+            else if (inputUnit == "24h")
+            {
+                if (inputValue > 12)
+                {
+                    return inputValue - 12;
+                }
+                else
+                {
+                    return inputValue;
+                }
+            }
+            throw new ArgumentException("Invalid input unit " + inputUnit);
+        }
+    }
+}
