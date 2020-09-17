e5c3f0d27c0f7d526f28455aabb9a4846fe9db34
Kamil Rybski <rybski.kamil@gmail.com>
2020-09-17 20:18:06 +0200

    modul5

---
+++ modul5/UnitConvert.Logic/IConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    public interface IConverter
+    {
+        string Name { get; }
+        List<string> Units { get; }
+        decimal Convert(string unitFrom, string unitTo, decimal valueToConvert);
+        decimal Convert(object p1, object p2, decimal inputValue);
+    }
+}
